02952a809fb950944d33f5c1ed01f190fbc89737
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-03 00:23:49 -0300

    Created grace/main.py at Timestamp: 2018-06-03 03:23:49

---
+++ grace/main.py
@@ -0,0 +1,20 @@
+# kesha.grace.main.py
+from _spy.vitollino.main import Cena, Texto, Elemento
+
+linkBackdropcena = "https://universoracionalista.org/wp-content/uploads/2017/05/galaxias.jpg"
+linkmarilyn = "https://activufrj.nce.ufrj.br/file/pedropeclat/WhatsApp_Image_2018-05-22_at_11.54.54.jpeg?disp=inline"
+
+
+def Defumado():
+    galaxia = cena(img = linkBackdropcena)
+    marilyn = elemento(img = linkmarilyn,tit="marilyn")
+    marilyn.entra(galaxia)
+    txtmarilyn = texto(galaxia,"Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
+    marilyn.vai=txtmarilyn.vai
+    galaxia.vai
+    
+
+    galaxia.vai()
+    
+    Defumado()
+    
