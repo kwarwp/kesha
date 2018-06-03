4f63c299b7d4813bbc65c4820e3da0c12a3d47b3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-03 00:36:05 -0300

    Saved grace/main.py at Timestamp: 2018-06-03 03:36:05

--- grace/main.py
+++ grace/main.py
@@ -5,16 +5,13 @@ linkBackdropcena = "https://universoracionalista.org/wp-content/uploads/2017/05/
 linkmarilyn = "https://activufrj.nce.ufrj.br/file/pedropeclat/WhatsApp_Image_2018-05-22_at_11.54.54.jpeg?disp=inline"
 
 
-def historia():
-    galaxia = cena(img = linkBackdropcena)
-    marilyn = elemento(img = linkmarilyn,tit="marilyn")
+def Defumado():
+    galaxia = Cena(img = linkBackdropcena)
+    marilyn = Elemento(img = linkmarilyn,tit = "marilyn")
     marilyn.entra(galaxia)
-    txtmarilyn = texto(galaxia,"Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
+    txtmarilyn = Texto(galaxia,"Ola, eu sou a ideia do bem e do mal.Esse e um jogo de escolhas, sobre a breve historia da humanidade.Para prosseguir, clique no universo.")
     marilyn.vai=txtmarilyn.vai
-    galaxia.vai
-    
-
     galaxia.vai()
     
-    historia()
+Defumado()
     
