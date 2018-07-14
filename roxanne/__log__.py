ff945ec363572c1ed5da495de42975bbf85925b9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-14 17:31:05 -0300

    Created roxanne/__log__.py at Timestamp: 2018-07-14 20:31:05

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Jul 14 2018 17:31:03.587 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  m6n=http://images.tcdn.com.br/img/img_prod/419216/balao_de_decoracao_de_festa_aniversario_marshall_patrulha_canina_1872_3_20180312095941.jpg
+           ^
+SyntaxError: invalid syntax
+'''},
