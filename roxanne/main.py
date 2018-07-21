3aeb23dce9c00d0695ff0d02683494ce54b37bc4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-21 20:16:23 -0300

    Saved roxanne/main.py at Timestamp: 2018-07-21 23:16:23

--- roxanne/main.py
+++ roxanne/main.py
@@ -5,20 +5,20 @@ STYLE ["width"] = 1150
 STYLE ["height"] = "600px"
 
 NOITE = "https://i.imgur.com/Cfqvyhg.png"
-FASE1 = "https://i.imgur.com/ayIr0WV.png"
-FASE2 = "https://i.imgur.com/UvM5aa6.png"
-FASE3 = "https://i.imgur.com/4nhx2A6.png"
-FASE4 = "https://i.imgur.com/jUrqxIe.png"
-FASE5 = "https://i.imgur.com/oVW6HwU.png"
-FASE6 = "https://i.imgur.com/dkM5AIW.png"
-FASE7 = "https://i.imgur.com/yjQaCPU.png"
-FASE8 = "https://i.imgur.com/V10xkZP.png"
-FASE9 = "https://i.imgur.com/tdOSHCw.png"
+FASE1 = "https://i.imgur.com/kZw8TRw.png"
+FASE2 = "https://i.imgur.com/wmDD3Ix.png"
+FASE3 = "https://i.imgur.com/83YrIHh.png"
+FASE4 = "https://i.imgur.com/ibovjJ1.png"
+FASE5 = "https://i.imgur.com/EBTCiQP.png"
+FASE6 = "https://i.imgur.com/NyPMdqD.png"
+FASE7 = "https://i.imgur.com/4dK7L1A.png"
+FASE8 = "https://i.imgur.com/Aw6x1kC.png"
+FASE9 = "https://i.imgur.com/uqRq6nc.png"
 
 def tela1():
     noite = Cena(img = NOITE)
     noite.vai()
-    fase1 = Elemento(img = FASE1, tit = "SIMETRICO 3 X 4", style = dict(left=500, top="100px", width=240, height="320px"))
+    fase1 = Elemento(img = FASE1, tit = "SIMETRICO 3 X 4", style = dict(left=200, top="100px", width=540, height="320px"))
     fase1.entra (noite)
       
 tela1 ()
