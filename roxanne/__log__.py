e033f5055805f6febf5f3ca7da182b9b77bf2242
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-22 17:50:53 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-07-22 20:50:53

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -618,3 +618,21 @@ IndentationError: unexpected indent
     self.linha1.entra(self.fase1)
 AttributeError: 'Tabuleiro' object has no attribute 'linha1'
 '''},
+{'date': 'Sun Jul 22 2018 17:50:46.652 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 152
+    Tabuleiro ()
+  module <module> line 99
+    self.linhaA4.entra(self.fase1)
+AttributeError: 'Tabuleiro' object has no attribute 'linhaA4'
+'''},
