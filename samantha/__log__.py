cf618e3c3d5d13a8369a9b9d3939eedef0add46f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-22 20:55:36 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-22 23:55:36

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -6,3 +6,21 @@
    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Jul 22 2018 20:55:32.776 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 158
+    Tabuleiro ()
+  module <module> line 27
+    self.linhaA1 = Elemento(QBRANCO, style = dict(
+NameError: name 'QBRANCO' is not defined
+'''},
