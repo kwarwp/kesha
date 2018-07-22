a368210d472c6681ca110267d0da107ab8b598d3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-22 19:08:30 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-07-22 22:08:30

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -654,3 +654,21 @@ AttributeError: 'Tabuleiro' object has no attribute 'linhaA4'
     self.txterro = Texto(linhaA2,"Ops!!")
 NameError: name 'linhaA2' is not defined
 '''},
+{'date': 'Sun Jul 22 2018 19:08:25.540 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 158
+    Tabuleiro ()
+  module <module> line 43
+    self.txterro = Texto(linhaA1,"Ops!!")
+NameError: name 'linhaA1' is not defined
+'''},
