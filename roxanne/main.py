3a4498f194dd5d705290f2fa26fec793465ff484
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-22 15:08:03 -0300

    Saved roxanne/main.py at Timestamp: 2018-07-22 18:08:03

--- roxanne/main.py
+++ roxanne/main.py
@@ -4,16 +4,15 @@ from _spy.vitollino.main import STYLE
 STYLE ["width"] = 1150
 STYLE ["height"] = "600px"
 
-NOITE = "https://i.imgur.com/Cfqvyhg.png"
-FASE1 = "https://i.imgur.com/kZw8TRw.png"
-FASE2 = "https://i.imgur.com/wmDD3Ix.png"
-FASE3 = "https://i.imgur.com/83YrIHh.png"
-FASE4 = "https://i.imgur.com/ibovjJ1.png"
-FASE5 = "https://i.imgur.com/EBTCiQP.png"
-FASE6 = "https://i.imgur.com/NyPMdqD.png"
-FASE7 = "https://i.imgur.com/4dK7L1A.png"
-FASE8 = "https://i.imgur.com/Aw6x1kC.png"
-FASE9 = "https://i.imgur.com/uqRq6nc.png"
+FASE1 = "https://i.imgur.com/pmlIiGt.png"
+FASE2 = "https://i.imgur.com/DailNDQ.png"
+FASE3 = "https://i.imgur.com/XWPxvYy.png"
+FASE4 = "https://i.imgur.com/KN4hH9Z.png"
+FASE5 = "https://i.imgur.com/mlBejyP.png"
+FASE6 = "https://i.imgur.com/VgB7jA0.png"
+FASE7 = "https://i.imgur.com/BKbYEGn.png"
+FASE8 = "https://i.imgur.com/zFlVIXy.png"
+FASE9 = "https://i.imgur.com/HyW0l5d.png"
 QBRANCO ="https://i.imgur.com/9PMJ1tD.png"
 QAZUL = "https://i.imgur.com/HUkZFHm.png"
 QVERDE = "https://i.imgur.com/hd3ofzP.png"
@@ -22,13 +21,13 @@ QSIMBOLO = "https://i.imgur.com/XnMRw3u.png"
 
 class Tabuleiro:
     def __init__(self):
-        self.noite = Cena(img = NOITE)
+        self.fase1 = Cena(img = FASE1)
         self.noite.vai()
-        self.fase1 = Elemento(img = FASE1, tit = "SIMETRICO 3 X 4", style = dict(left=200, top="100px", width=540, height="320px"))
-        self.fase1.entra(self.noite)
-        self.qsimbolo = Elemento(img = QSIMBOLO, style = dict(left=100, top="50px", width=40, height="40px"))
-        self.qsimbolo.entra(self.fase1)
-HELLO 
+        #self.fase1 = Elemento(img = FASE1, tit = "SIMETRICO 3 X 4", style = dict(left=200, top="100px", width=540, height="320px"))
+        #self.fase1.entra(self.noite)
+        #self.qsimbolo = Elemento(img = QSIMBOLO, style = dict(left=100, top="50px", width=40, height="40px"))
+        #self.qsimbolo.entra(self.fase1)
+
 """### TABULEIRO RESPOSTA ####
         TBRX, TBRY = 200, 100
         self.r0= Elemento(QBRANCO,style=dict(width=TBRX, height=TBRY, left=751, top=55))
