03cd4d19e168b05b25ba4103ba732dd9c4096ea0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-22 20:50:27 -0300

    Created samantha/__log__.py at Timestamp: 2018-07-22 23:50:27

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Jul 22 2018 20:50:22.982 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 26
+  <table class=MsoTableGrid border=1 cellspacing=0 cellpadding=0
+   ^
+SyntaxError: invalid syntax
+'''},
