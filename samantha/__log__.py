bc91e1e5e31ba6c8033744e874df173cd07badbc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 19:39:41 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-23 22:39:40

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -164,3 +164,21 @@ SyntaxError: invalid syntax
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Jul 23 2018 19:39:37.957 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 254
+    Tabuleiro ()
+  module <module> line 202
+    self.tabuleiro[nome].elt.onclick = alert(self.tabuleiro[nome])         
+AttributeError: 'Tabuleiro' object has no attribute 'tabuleiro'
+'''},
