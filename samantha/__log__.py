5005681b26aeb1bb2cc84121594d1e4bf6189ac3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 19:32:25 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-23 22:32:25

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -104,3 +104,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Jul 23 2018 19:32:23.758 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 190
+  Desafio_Cor_Fase1 = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"]
+  ^
+IndentationError: unexpected indent
+'''},
