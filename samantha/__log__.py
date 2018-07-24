58477d656bb765e2b29be02b02f2039b421f5bfb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 21:00:45 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-24 00:00:44

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -200,3 +200,21 @@ AttributeError: 'Tabuleiro' object has no attribute 'tabuleiro'
     self.tabuleiro[nome].elt.onclick = recoloca_clique_aqui         
 AttributeError: 'Tabuleiro' object has no attribute 'tabuleiro'
 '''},
+{'date': 'Mon Jul 23 2018 21:00:41.694 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 152
+    Tabuleiro ()
+  module <module> line 101
+    self.escolha_cria()
+AttributeError: 'Tabuleiro' object has no attribute 'escolha_cria'
+'''},
