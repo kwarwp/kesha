4118bc53923bb1fe0d68e7a06e210202d51b7065
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-24 16:49:55 -0300

    Saved libby/main.py at Timestamp: 2018-07-24 19:49:54

--- libby/main.py
+++ libby/main.py
@@ -12,6 +12,7 @@ silv = dict(color=color.blue, opacity=0.4)
 hull = dict(color=color.yellow)
 #s = shapes.rectangle(width=10, height=6)
 #h1=box(pos=(1.5, -0.5, 0),size=(1,0.1,0.5), axis=(0,1,0), color=color.blue)
+
 """
 #h2=sphere(pos=(1.5, 0.5, 0),size=(1,0.1,0.5), axis=(0,1,0), color=color.black)
 #pyramid(pos=(1, -0.5, 0),size=(0.5,0.1,1), axis=(0,1,0), color=color.black)
@@ -25,8 +26,8 @@ rec0=box(pos=(5, 0.2, 0), size=(1,1,2), **silv)
 rec1=box(pos=(6.2, 0.2, 0), size=(1,1,2), **silv)
 rec1=box(pos=(6.2, 0.2, 0), size=(1,1,2), **silv)
 simetria = [rec0, rec1, rec2]
+
 """
-# --- portholes ---
 simetria = [box(pos=(x/2+2.2, 0.15, 0.9), size=(0.3,0.3,0.3), **silv) for x in range(5)]
 php = [sphere(pos=(x/2+2.2, 0.15, -0.9), size=(0.3,0.3,0.3), **silv) for x in range(5)]
 pts = [h1, h2, aft, fr, body, caft, cfor, cbody]+per+php+phs
@@ -37,3 +38,4 @@ def sail():
     sup.pos += vec(0.2,0,0)
     sup.rotate(angle=0.02,
            axis=
+"""
