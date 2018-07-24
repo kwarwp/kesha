561f62c1f46b5607289f60a3085d186efb0036ed
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-24 17:48:36 -0300

    Saved libby/main.py at Timestamp: 2018-07-24 20:48:36

--- libby/main.py
+++ libby/main.py
@@ -10,17 +10,8 @@ scene.width = 600
 scene.height = 600
 silv = dict(color=color.blue)
 
-
 simetria1=box(pos=(2, 0, 0), size=(0.3,0.3,0.3) , color=color.blue)
-simetria2=box(pos=(2, 0, 0), size=(0.3,0.3,0.3) , color=color.blue)
+simetria2=box(pos=(0, 0, -2), size=(0.3,0.3,0.3) , color=color.blue)
 simetria = [box(pos=(x/2+2.2, 0.15, 0.9), size=(0.3,0.3,0.3), **silv) for x in range(4)]
 
 Q1 = [simetria]+simetria1+simetria2
-#sup = compound(pts, pos=(-1,0,0))
-"""
-def sail():
-    rate(2, sail)
-    sup.pos += vec(0.2,0,0)
-    sup.rotate(angle=0.02,
-           axis=
-"""
