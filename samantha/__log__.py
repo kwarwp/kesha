1591262c6625911c3fb74025746518dad31fdf91
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-24 01:31:40 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-24 04:31:40

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -512,3 +512,23 @@ Exception: self.$events is undefined
   module '$exec_9052' line 161
 Tabuleiro ()
 '''},
+{'date': 'Tue Jul 24 2018 01:31:36.652 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 161
+    Tabuleiro ()
+  module <module> line 95
+    self.linhaA1.elt.onclick = escolha_cria(self.fase1)
+  module <module> line 30
+    self.linhaA1.entra(self.fase1)
+AttributeError: 'Cena' object has no attribute 'linhaA1'
+'''},
