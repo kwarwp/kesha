1e3b074ce57d2aa42ac635865d2027d6439cd68c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-24 02:03:46 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-24 05:03:46

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -604,3 +604,21 @@ NameError: name 'ALIMENTO_FASE1_1' is not defined
     Pilha_Cartas = [EXCREMENTO_FASE1_16, DIVERSAO_FASE1_15, DESCANSO_FASE1_14, ALIMENTO_FASE1_13,\
 NameError: name 'EXCREMENTO_FASE1_16' is not defined
 '''},
+{'date': 'Tue Jul 24 2018 02:03:42.387 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 226
+    Tabuleiro()
+  module <module> line 66
+    self.casa0 = Elemento(RESP_SORRISOMAIS_FASE1, tit='0_0', style=dict(
+NameError: name 'RESP_SORRISOMAIS_FASE1' is not defined
+'''},
