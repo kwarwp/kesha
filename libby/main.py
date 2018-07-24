61d513fc7b94d435d8ee74652a6989b357cc7028
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-24 17:13:22 -0300

    Saved libby/main.py at Timestamp: 2018-07-24 20:13:22

--- libby/main.py
+++ libby/main.py
@@ -10,19 +10,14 @@ scene.width = 600
 scene.height = 600
 silv = dict(color=color.blue)
 hull = dict(color=color.yellow)
-"""
-body=cylinder(pos=(2, 0, 0), size=(4,1,2) , color=color.yellow)
-"""
-rec0=box(pos=(5, 0.2, 0), size=(4,4,4), **silv)
-#rec1=box(pos=(6.2, 0.2, 0), size=(4,4,4), **silv)
-#rec1=box(pos=(7.2, 0.2, 0), size=(4,4,4), **silv)
-#simetria = [rec0, rec1, rec2]
 
-simetria = [box(pos=(x/2+2.2, 0.15, 0.9), size=(0.3,0.3,0.3), **silv) for x in range(5)]
-#php = [sphere(pos=(x/2+2.2, 0.15, -0.9), size=(0.3,0.3,0.3), **silv) for x in range(5)]
-#pts = [h1, h2, aft, fr, body, caft, cfor, cbody]+per+php+phs
+simetria1=box(pos=(2, 0, 0), size=(4,1,2) , color=color.blue)
+simetria2=box(pos=(2, 0, 0), size=(4,1,2) , color=color.blue)
+simetria = [box(pos=(x/2+2.2, 0.15, 0.9), size=(0.3,0.3,0.3), **silv) for x in range(4)]
+
+Q1 = [simetria]+simetria1+simetria2
 #sup = compound(pts, pos=(-1,0,0))
-
+"""
 def sail():
     rate(2, sail)
     sup.pos += vec(0.2,0,0)
