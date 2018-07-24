f7845ddc08d148fdb658594eab58bbb5633f6d72
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-24 02:36:01 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-24 05:36:01

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -730,3 +730,21 @@ NameError: name 'FUNDO_VAZIO' is not defined
     self.cliqueaqui.elt.onclick = remove_clique_aqui        
 AttributeError: 'Tabuleiro' object has no attribute 'cliqueaqui'
 '''},
+{'date': 'Tue Jul 24 2018 02:35:56.512 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 203
+    Tabuleiro()
+  module <module> line 107
+    self.cliqueaqui.elt.onclick = remove_clique_aqui        
+AttributeError: 'Tabuleiro' object has no attribute 'cliqueaqui'
+'''},
