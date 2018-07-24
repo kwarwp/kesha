f258d0681270af0da0c8f7cbed2899a34d9aadc0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-24 01:57:14 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-24 04:57:14

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -532,3 +532,21 @@ Tabuleiro ()
     self.linhaA1.entra(self.fase1)
 AttributeError: 'Cena' object has no attribute 'linhaA1'
 '''},
+{'date': 'Tue Jul 24 2018 01:57:10.904 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 226
+    Tabuleiro()
+  module <module> line 48
+    self.tabela_fase1 = tabelafase1 = Cena(img=TABELAFASE1)
+NameError: name 'TABELAFASE1' is not defined
+'''},
