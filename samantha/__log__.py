7ea33819b3563f950628a2810d26a46aeb387af2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-24 02:58:41 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-24 05:58:40

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -766,3 +766,21 @@ AttributeError: 'Tabuleiro' object has no attribute 'cliqueaqui'
     self.cliqueaqui.entra (tabelafase1)
 AttributeError: 'Tabuleiro' object has no attribute 'cliqueaqui'
 '''},
+{'date': 'Tue Jul 24 2018 02:58:37.448 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 204
+    Tabuleiro()
+  module <module> line 87
+    self.tabuleiro[nome] = Elemento(FUNDO_BRANCO, tit=nome+"_", style=dict(
+UnboundLocalError: local variable 'coluna' referenced before assignment
+'''},
