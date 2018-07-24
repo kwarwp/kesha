7b3a00d0784fb7da447af849d8bbebb3941b0908
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-23 23:31:56 -0300

    Saved samantha/__log__.py at Timestamp: 2018-07-24 02:31:56

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -429,3 +429,21 @@ NameError: name 'linhaA1' is not defined
     self.quadros[quadro] = Elemento(Desafio_Cor_Fase1[quadro], style = dict(left= 114+lef, top= "148px", width=80, height="80px"))
 AttributeError: 'Tabuleiro' object has no attribute 'quadros'
 '''},
+{'date': 'Mon Jul 23 2018 23:31:53.140 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 157
+    Tabuleiro ()
+  module <module> line 106
+    colorir(self)
+NameError: name 'colorir' is not defined
+'''},
