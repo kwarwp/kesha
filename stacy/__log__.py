c1974817cb7500e76410a7df12c61957342b1a5e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 18:12:51 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-25 21:12:51

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -359,3 +359,21 @@ IndentationError: unexpected indent
     for nome in tabuleiro["direita"]:
 NameError: name 'tabuleiro' is not defined
 '''},
+{'date': 'Wed Jul 25 2018 15:12:52.421 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 134
+    Tabuleiro()
+  module <module> line 102
+    self.tabuleiro["direita"][nome].elt.onclick = move_carta   
+KeyError: 0_0
+'''},
