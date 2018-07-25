e44650c8cce75bea910b8779bfa9132023a250a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 17:55:08 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-25 20:55:08

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -190,3 +190,21 @@ AttributeError: 'Tabuleiro' object has no attribute 'tabuleiro'
                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jul 25 2018 14:55:11.495 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 129
+    Tabuleiro()
+  module <module> line 97
+    self.tabuleiro[esquerda[nome]] = Elemento(FUNDO_BRANCO, tit=nome+"_", style=dict(
+NameError: name 'esquerda' is not defined
+'''},
