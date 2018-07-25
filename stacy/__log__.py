f6ae58cebaae3913e4d791f528cc32580ddbe48d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 17:45:28 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-25 20:45:27

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -111,3 +111,21 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Jul 25 2018 14:45:30.971 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 129
+    Tabuleiro()
+  module <module> line 107
+    nome = "{}_{}".format(linha, coluna)
+NameError: name 'linha' is not defined
+'''},
