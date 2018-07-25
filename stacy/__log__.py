c7ca551d559c6bcfea70acd2f5b74edb90abe493
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 18:58:56 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-25 21:58:55

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -405,3 +405,21 @@ SyntaxError: invalid syntax
                                                                                                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jul 25 2018 15:59:00.882 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 132
+    Tabuleiro()
+  module <module> line 62
+    Pilha_Cartas.append(QZUL)#lista das cartas
+NameError: name 'QZUL' is not defined
+'''},
