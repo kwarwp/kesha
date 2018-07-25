9e11298ed5df0c9b40a38cf3860ac47733edc27b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 09:21:31 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-25 12:21:30

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -56,3 +56,21 @@ NameError: name 'FUNDO_BRANCO' is not defined
                                                                                                 ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Jul 25 2018 06:21:23.953 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 118
+    Tabuleiro()
+  module <module> line 53
+    self.tabela_fase1 = tabelafase1 = Cena(img=FUNDO)
+NameError: name 'FUNDO' is not defined
+'''},
