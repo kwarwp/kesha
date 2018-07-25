5d1216a98779647def3b0c3f5c831156543571d3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 10:02:52 -0300

    Saved libby/__log__.py at Timestamp: 2018-07-25 13:02:52

--- libby/__log__.py
+++ libby/__log__.py
@@ -507,3 +507,21 @@ SyntaxError: non-keyword arg after keyword arg
     scene.background= color.black #"rgba(0, 0, 200, 0.5)"
 NameError: name 'scene' is not defined
 '''},
+{'date': 'Wed Jul 25 2018 07:02:46.488 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    _gs=Glow('pydiv')
+  module _spy.vpython.main line 20
+    document[container] <= html.DIV(Id=glowid, Class="glowscript")
+KeyError: pydiv
+'''},
