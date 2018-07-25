ebe6546d65c668de331bbcbd264c320ef3dc755a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 16:03:48 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-25 19:03:47

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -81,3 +81,19 @@ NameError: name 'FUNDO' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Jul 25 2018 13:03:48.899 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    cliques = [ (3,verbos_altos),(2,verbos_medios),(1,verbos_fracos)]
+NameError: name 'verbos_altos' is not defined
+'''},
