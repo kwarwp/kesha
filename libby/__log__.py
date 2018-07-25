01f2e277ce00ada9e57bd5bd6e324bb83b8430ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 09:57:02 -0300

    Saved libby/__log__.py at Timestamp: 2018-07-25 12:57:02

--- libby/__log__.py
+++ libby/__log__.py
@@ -491,3 +491,19 @@ SyntaxError: non-keyword arg after keyword arg
                                                             ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Wed Jul 25 2018 06:56:55.860 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    scene.background= color.black #"rgba(0, 0, 200, 0.5)"
+NameError: name 'scene' is not defined
+'''},
