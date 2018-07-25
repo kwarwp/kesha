ffdcb5dc816ee187c30657596cf8d9f27afb8ceb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 18:47:40 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-25 21:47:40

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -398,3 +398,10 @@ SyntaxError: invalid syntax
                                                                                                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jul 25 2018 15:47:47.104 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  if((tabuleiro_target == "esquerda" and self.tabuleiro["direita"][casa_destino].img.ocupado == 1) 
+                                                                                                                                         ^
+SyntaxError: invalid syntax
+'''},
