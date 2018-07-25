94846404a448ddf18331401c567aa2a2f0e1fc96
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 18:12:45 -0300

    Saved stacy/__log__.py at Timestamp: 2018-07-25 21:12:45

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -341,3 +341,21 @@ KeyError: 3_2
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Jul 25 2018 15:11:33.761 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 132
+    Tabuleiro()
+  module <module> line 123
+    for nome in tabuleiro["direita"]:
+NameError: name 'tabuleiro' is not defined
+'''},
