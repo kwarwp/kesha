27bb5506fc74d319c80ab7371e0fc2778c10838b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-25 09:52:00 -0300

    Saved libby/main.py at Timestamp: 2018-07-25 12:52:00

--- libby/main.py
+++ libby/main.py
@@ -10,8 +10,8 @@ scene.width = 600
 scene.height = 600
 silv = dict(color=color.blue)
 
-simetria1=box(pos=(2.1, 0.2, 0), size=(0.4,0.4,0.4) , **silv)
-simetria2=box(pos=(3.6, 0.2, 1.5), size=(0.4,0.4,0.4) , **silv)
+simetria1=box(pos=(2.1, 0.2, 0), size=(0.4,0.4,0.4) , silv)
+simetria2=box(pos=(3.6, 0.2, 1.5), size=(0.4,0.4,0.4) , silv)
 simetria = [box(pos=(x/2+2.2, 0.15, 0.9), size=(0.4,0.4,0.4), **silv) for x in range(4)]
 
 Q1 = [simetria]+simetria1+simetria2
