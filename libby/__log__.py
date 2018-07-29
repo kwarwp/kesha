dc301ff978ca1a3533aae685157d4cde421006ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-29 13:29:36 -0300

    Saved libby/__log__.py at Timestamp: 2018-07-29 16:29:36

--- libby/__log__.py
+++ libby/__log__.py
@@ -1109,3 +1109,19 @@ TypeError: __init__() takes 4 positional argument but more were given
     kwargs[_key] = vec(*kwargs[_key])._vec
 TypeError: __init__() takes 4 positional argument but more were given
 '''},
+{'date': 'Sun Jul 29 2018 13:29:33.772 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 13
+    simetria1=box(pos=(1.2, 2, 0), size=(1,1,1) , **silv, color=red)
+NameError: name 'red' is not defined
+'''},
