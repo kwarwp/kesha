fb7db8a3f381ad9e4dd8cd9ed6cb6fb6462e3807
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-29 15:23:30 -0300

    Saved libby/main.py at Timestamp: 2018-07-29 18:23:30

--- libby/main.py
+++ libby/main.py
@@ -8,14 +8,14 @@ scene=canvas()
 scene.background= color.black #"rgba(0, 0, 200, 0.5)"
 scene.width = 800
 scene.height = 600
-silv = dict(color=color.blue)
+bloco = dict(color=color.blue)
 
-simetria1=box(pos=(4.2, -2, 0), size=(1,1,1) , **silv)
-simetria2=box(pos=(1.8, 1.6, 0), size=(1,1,1) , **silv)
-simetria = [box(pos=(3, x*(1.2)-2, 0), size=(1,1,1), **silv) for x in range(4)]
+simetria1=box(pos=(4.2, -2, 0), size=(1,1,1) , **bloco)
+simetria2=box(pos=(1.8, 1.6, 0), size=(1,1,1) , **bloco)
+simetria = [box(pos=(3, x*(1.2)-2, 0), size=(1,1,1), **bloco) for x in range(4)]
 
 pts = [simetria1, simetria2]+simetria
-sup = compound(pts, pos=vec(2,0,0), axis=vec(3,1,-1))
+sup = compound(pts, pos=vec(2,0,0), axis=vec(1,0,-1))
 
 #def sail():
     #rate(2, sail)
