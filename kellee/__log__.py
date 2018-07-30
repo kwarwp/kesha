ebd3d2f3e5097df100fb44be0ecf74517b3580ff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-30 11:38:02 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-30 14:38:01

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -280,3 +280,23 @@ KeyError: py3d
     doc['py3d'].html=''
 KeyError: py3d
 '''},
+{'date': 'Mon Jul 30 2018 08:37:58.15 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 114
+    Tabuleiro()
+  module <module> line 111
+    self._3d_()
+  module <module> line 20
+    doc['py3d'].html=''
+KeyError: py3d
+'''},
