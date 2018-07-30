2ca9addb6abea081b9161476910fe83b72316d09
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-30 11:15:10 -0300

    Saved kellee/main.py at Timestamp: 2018-07-30 14:15:10

--- kellee/main.py
+++ kellee/main.py
@@ -3,9 +3,6 @@ from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 from browser import html, document, alert, doc
 #doc["glowscript"] <= html.H1('XXXXXXXXX -------------- XXXXXXXXXXX')
 from _spy.vpython.main import *
-doc['py3d'].html=''
-_gs=Glow('py3d')
-scene=canvas()
 #scene.background= color.white
 #scene.width = 800
 #scene.height = 600
@@ -19,6 +16,18 @@ QVERMELHO = "https://i.imgur.com/K0YpYsi.png"
 QVERDE = "https://i.imgur.com/hd3ofzP.png"
 
 class Tabuleiro:
+    def _3d_(self):
+        doc['py3d'].html=''
+        _gs=Glow('py3d')
+        scene=canvas()
+
+        bloco = dict(color=color.blue)
+        simetria1=box(pos=(4.2, -2, 0), size=(1,1,1) , **bloco)
+        simetria2=box(pos=(1.8, 1.6, 0), size=(1,1,1) , **bloco)
+        simetria = [box(pos=(3, x*(1.2)-2, 0), size=(1,1,1), **bloco) for x in range(4)]
+        pts = [simetria1, simetria2]+simetria
+        sup = compound(pts, pos=vec(2,0,0), axis=vec(4,0,-1))
+
     def __init__(self):
         self.fase1 = Cena(img = FASE1)
         self.linhaA1 = Elemento(QBRANCO, style = dict(tit = "py3d",
