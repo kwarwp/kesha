af978a78a36536fd035b752f8a21d80c6cf8a996
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-29 22:42:57 -0300

    Saved sara/main.py at Timestamp: 2018-07-30 01:42:57

--- sara/main.py
+++ sara/main.py
@@ -1,8 +1,8 @@
 # kesha.sara.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
-from browser import html, document, alert
+from browser import html, document, alert, doc
+#doc["glowscript"] <= html.H1('XXXXXXXXX -------------- XXXXXXXXXXX')
 from _spy.vpython.main import *
-
 doc['pydiv'].html=''
 _gs=Glow('pydiv')
 scene=canvas()
@@ -10,26 +10,23 @@ scene.background= color.white
 scene.width = 800
 scene.height = 600
 
-"""
-STYLE["width"]=800
-STYLE["height"]="600px"
-FUNDO = "https://i.imgur.com/EzWk7Jl.jpg" 
-#FUNDO_BRANCO = "https://i.imgur.com/UXD0mzp.png"
-QAZUL = "https://i.imgur.com/lWDGIvc.jpg"
-QVERMELHO = "https://i.imgur.com/K0YpYsi.png"
-QVERDE = "https://i.imgur.com/hd3ofzP.png"
-#crivo#
-#pontos_altos = ""
-#pontos_medios = ""
-#pontos_fracos = ""
-"""
+
+
 bloco = dict(color=color.blue)
 simetria1=box(pos=(4.2, -2, 0), size=(1,1,1) , **bloco)
 simetria2=box(pos=(1.8, 1.6, 0), size=(1,1,1) , **bloco)
 simetria = [box(pos=(3, x*(1.2)-2, 0), size=(1,1,1), **bloco) for x in range(4)]
 pts = [simetria1, simetria2]+simetria
 sup = compound(pts, pos=vec(2,0,0), axis=vec(4,0,-1))
-"""
+
+
+STYLE["width"]=800
+STYLE["height"]="600px"
+FUNDO = "https://i.imgur.com/EzWk7Jl.jpg" 
+QAZUL = "https://i.imgur.com/lWDGIvc.jpg"
+QVERMELHO = "https://i.imgur.com/K0YpYsi.png"
+QVERDE = "https://i.imgur.com/hd3ofzP.png"
+
 class Tabuleiro:
 
     def __init__ (self):
@@ -102,4 +99,4 @@ class Tabuleiro:
         
 Tabuleiro()
 
-"""
+
