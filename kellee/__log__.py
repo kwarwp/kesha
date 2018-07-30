be26d9fb8b3792fa06bc9ffcf09d7a0e6ee02e2c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-30 19:16:13 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-30 22:16:13

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -660,3 +660,23 @@ SyntaxError: invalid syntax
                                                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jul 30 2018 19:12:32.846 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 135
+    Tabuleiro()
+  module <module> line 43
+    self.cria_tabuleiro(col=3, lin=4)
+  module <module> line 56
+    INVENTARIO.score(casa="centro_9_9", carta="carta_99", move="INICIA", ponto=0, valor="N", _level=1)
+NameError: name 'INVENTARIO' is not defined
+'''},
