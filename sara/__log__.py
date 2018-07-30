19e8b2f5fd778d67e44a4eb8c41c5ec814dabfdf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-29 22:29:56 -0300

    Saved sara/__log__.py at Timestamp: 2018-07-30 01:29:56

--- sara/__log__.py
+++ sara/__log__.py
@@ -84,3 +84,19 @@ Exception: Cannot set property 'canvas_selected' of undefined
   module '$exec_1883' line 8
 scene=canvas()
 '''},
+{'date': 'Sun Jul 29 2018 22:29:53.475 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    doc['pydiv'].html=''
+NameError: name 'doc' is not defined
+'''},
