3c27adec32ad882c75739f835754acb657eecd7f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-30 18:29:36 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-30 21:29:36

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -598,3 +598,27 @@ KeyError: esquerda
     self.elemento.entra(parte)
 AttributeError: 'Tabuleiro' object has no attribute 'elemento'
 '''},
+{'date': 'Mon Jul 30 2018 18:25:56.67 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 126
+    Tabuleiro()
+  module <module> line 43
+    self.cria_tabuleiro(col=3, lin=4)
+  module <module> line 60
+    self.casa["esquerda"][nome] = Casa(self, nome=nome, linha=linha_, coluna=coluna_)
+  module <module> line 111
+    self.tabuleiro.entra(self)
+  module <module> line 63
+    self.elemento.entra(parte)
+AttributeError: 'Cena' object has no attribute 'entra'
+'''},
