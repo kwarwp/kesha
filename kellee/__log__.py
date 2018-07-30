1fd904c50467b62b939d0cea3f8daac0331271dc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-30 18:26:06 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-30 21:26:05

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -554,3 +554,23 @@ KeyError: py3d
         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jul 30 2018 18:22:24.810 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 127
+    Tabuleiro()
+  module <module> line 41
+    self.cria_tabuleiro(col=3, lin=4)
+  module <module> line 61
+    self.casa["esquerda"][nome] = Casa(self, nome=nome, linha=linha_, coluna=coluna_)
+KeyError: esquerda
+'''},
