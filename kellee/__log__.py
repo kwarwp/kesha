06c819aab4f4df657a1696f250961dd48dd93bad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-30 09:59:29 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-30 12:59:29

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -33,3 +33,21 @@ NameError: name 'Game' is not defined
     fase1.vai()
 NameError: name 'fase1' is not defined
 '''},
+{'date': 'Mon Jul 30 2018 06:59:25.428 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    game()
+  module <module> line 22
+    linha1.vai()
+NameError: name 'linha1' is not defined
+'''},
