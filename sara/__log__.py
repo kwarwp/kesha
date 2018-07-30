1d63d1c9376e6675ea8e502f93e1b7d56c41fe2e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-29 22:18:26 -0300

    Saved sara/__log__.py at Timestamp: 2018-07-30 01:18:26

--- sara/__log__.py
+++ sara/__log__.py
@@ -34,3 +34,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Jul 29 2018 22:18:24.583 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 79
+  Pilha_Cartas_top = 30
+  ^
+IndentationError: unexpected indent
+'''},
