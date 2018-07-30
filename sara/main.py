9339d9ef3c4ca0b4b4a1064f35efa27034a035f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-29 22:27:30 -0300

    Saved sara/main.py at Timestamp: 2018-07-30 01:27:30

--- sara/main.py
+++ sara/main.py
@@ -1,6 +1,15 @@
 # kesha.sara.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 from browser import html, document, alert
+from _spy.vpython.main import *
+
+doc['pydiv'].html=''
+_gs=Glow('pydiv')
+scene=canvas()
+#scene.background= color.white
+#scene.width = 800
+#scene.height = 600
+
 
 STYLE["width"]=800
 STYLE["height"]="600px"
@@ -13,6 +22,12 @@ QVERDE = "https://i.imgur.com/hd3ofzP.png"
 #pontos_altos = ""
 #pontos_medios = ""
 #pontos_fracos = ""
+bloco = dict(color=color.blue)
+simetria1=box(pos=(4.2, -2, 0), size=(1,1,1) , **bloco)
+simetria2=box(pos=(1.8, 1.6, 0), size=(1,1,1) , **bloco)
+simetria = [box(pos=(3, x*(1.2)-2, 0), size=(1,1,1), **bloco) for x in range(4)]
+pts = [simetria1, simetria2]+simetria
+sup = compound(pts, pos=vec(2,0,0), axis=vec(4,0,-1))
 
 class Tabuleiro:
 
@@ -85,3 +100,4 @@ class Tabuleiro:
             tabelafase1.vai()
         
 Tabuleiro()
+
