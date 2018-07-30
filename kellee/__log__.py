870d979f22688691d0c545a388029b09c0e9ae08
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-30 10:26:13 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-30 13:26:12

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -107,3 +107,23 @@ NameError: name 'fase1' is not defined
     doc['py3d'].html=''
 NameError: name 'doc' is not defined
 '''},
+{'date': 'Mon Jul 30 2018 07:26:09.480 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    game()
+  module <module> line 36
+    self._3d_()
+  module <module> line 18
+    doc['py3d'].html=''
+NameError: name 'doc' is not defined
+'''},
