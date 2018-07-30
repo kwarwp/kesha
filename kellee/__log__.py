b0cc078262bad98580a9c31fc328238fb8218690
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-30 18:31:43 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-30 21:31:42

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -622,3 +622,27 @@ AttributeError: 'Tabuleiro' object has no attribute 'elemento'
     self.elemento.entra(parte)
 AttributeError: 'Cena' object has no attribute 'entra'
 '''},
+{'date': 'Mon Jul 30 2018 18:28:01.946 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 126
+    Tabuleiro()
+  module <module> line 48
+    Carta(self, "carta_{}".format(i))
+  module <module> line 76
+    self.cria_carta(carta_id)
+  module <module> line 83
+    a_carta_a_ser_empilhada.entra(self.tabuleiro)
+  module _spy.vitollino.main line 459
+    cena <= self
+TypeError: '<=' not supported between instances of 'Tabuleiro' and 'Elemento'
+'''},
