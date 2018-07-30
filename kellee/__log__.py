0ccdf29069c16d743bc1362cdc886db24fb805a7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-07-30 10:50:57 -0300

    Saved kellee/__log__.py at Timestamp: 2018-07-30 13:50:57

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -172,3 +172,21 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Jul 30 2018 07:50:52.626 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 39
+    game()
+  module <module> line 38
+    self._3d_()
+AttributeError: 'game' object has no attribute '_3d_'
+'''},
