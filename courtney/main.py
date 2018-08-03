45366b8cbb940b8dd5d78df47428ff112e3522ec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-03 10:26:32 -0300

    Saved courtney/main.py at Timestamp: 2018-08-03 13:26:32

--- courtney/main.py
+++ courtney/main.py
@@ -16,20 +16,19 @@ from _spy.vpython.main import *
 class botoes():
     def frameBotoesTelaInicial(self):
             self.frame1= tk.Frame()
-            btn1= tk.Button(master=self.frame1, text="Inserir", command= self.processaInserir).pack(side=tk.LEFT, padx= 20, pady= 20)
-            btn2= tk.Button(master=self.frame1, text="Pesquisar", command= self.processaPesquisar).pack(side=tk.LEFT, padx= 20, pady= 20)
-            btn3= tk.Button(master=self.frame1, text="Editar",command= self.processaEditar).pack(side=tk.LEFT, padx= 20, pady= 20)
-            btn3= tk.Button(master=self.frame1, text="Listar",command= self.processaListar).pack(side=tk.LEFT, padx= 20, pady= 20)
+            btn1= tk.Button(master=self.frame1, text="Tentar novamente esta fase", command= self.processaInserir).pack(side=tk.LEFT, padx= 20, pady= 20)
+            btn2= tk.Button(master=self.frame1, text="Não quero jogar", command= self.processaPesquisar).pack(side=tk.LEFT, padx= 20, pady= 20)
+            btn3= tk.Button(master=self.frame1, text="Desisto",command= self.processaEditar).pack(side=tk.LEFT, padx= 20, pady= 20)
+            btn3= tk.Button(master=self.frame1, text="Terminei mas acho que está errado",command= self.processaListar).pack(side=tk.LEFT, padx= 20, pady= 20)
             return self.frame1       
          
+    def create_canvas_area(self):
+        lbl1= tk.Label(self.root, text="Sistema Gerenciador de Componentes do Caderno", font=("Helvetica", 20, "bold"), fg="blue").pack()
 
-"""def create_canvas_area(self):
-    lbl1= tk.Label(self.root, text="Sistema Gerenciador de Componentes do Caderno", font=("Helvetica", 20, "bold"), fg="blue").pack()
-         
-    self.frameBotoesTelaInicial().pack()
+        self.frameBotoesTelaInicial().pack()
              
              
-def processaInserir(self):
+"""def processaInserir(self):
     tkmsg.showinfo("Botão pressionado","Botão Inserir")
          
 def processaPesquisar(self):
