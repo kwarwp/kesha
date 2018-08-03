b75ac052b0b7601a0884fcb20e7bd429bd7e82e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-03 09:53:32 -0300

    Created courtney/__log__.py at Timestamp: 2018-08-03 12:53:32

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Aug 03 2018 09:53:27.384 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  botao1 = Button (janela.Text = "Tenta Novamente a fase")
+                               ^
+SyntaxError: keyword can't be an expression
+'''},
