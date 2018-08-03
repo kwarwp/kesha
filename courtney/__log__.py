d97848642871e2f813ac874e8f277c876c9ed6d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-03 09:55:12 -0300

    Saved courtney/__log__.py at Timestamp: 2018-08-03 12:55:12

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -6,3 +6,19 @@
                                ^
 SyntaxError: keyword can't be an expression
 '''},
+{'date': 'Fri Aug 03 2018 09:55:07.978 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    janela = Tk()
+NameError: name 'Tk' is not defined
+'''},
