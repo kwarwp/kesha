948d808c0ff41640b820a3234dffb7f315e6a1ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-03 20:45:22 -0300

    Created angie/__log__.py at Timestamp: 2018-08-03 23:45:22

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Aug 03 2018 20:45:15.72 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+   ^
+SyntaxError: invalid syntax
+'''},
