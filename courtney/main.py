aaebb1037a99db4ab30bf49d2b8ebc0a76a6d046
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-03 09:53:11 -0300

    Created courtney/main.py at Timestamp: 2018-08-03 12:53:10

---
+++ courtney/main.py
@@ -0,0 +1,14 @@
+# kesha.courtney.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE, INVENTARIO
+from browser import html, document, alert, doc
+from _spy.vpython.main import *
+from Tkinter import*
+janela = Tk()
+
+def name(event):
+    print ("olá")
+janela.geometry("100x300+100+100")
+botao1 = Button (janela.Text = "Tenta Novamente a fase")
+botao1.bind "<Button-1>", name)
+botao1.pack()
+janela.mainloop()
