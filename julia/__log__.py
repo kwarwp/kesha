b99624d8f63e24eef8bfeec368068a2155e01ccc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 15:29:29 -0300

    Saved julia/__log__.py at Timestamp: 2018-08-08 18:29:28

--- julia/__log__.py
+++ julia/__log__.py
@@ -147,3 +147,19 @@ NameError: name 'death' is not defined
                                                                                                                                                                                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 08 2018 15:30:12.723 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    aCasa = Cena(img = "https://ovelhinhodorpg.files.wordpress.com/2015/03/14_41_31_308_file.jpeg")
+TypeError: 'module' object is not callable
+'''},
