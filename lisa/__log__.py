88b4fa86cf2bb104b5fc11d7edef8e98bee90256
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 15:09:45 -0300

    Created lisa/__log__.py at Timestamp: 2018-08-08 18:09:44

---
+++ lisa/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Wed Aug 08 2018 08:09:55.755 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    jardim1=cena(img="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Butchardgardens.jpg/1200px-Butchardgardens.jpg")
+NameError: name 'cena' is not defined
+'''},
