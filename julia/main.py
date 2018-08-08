5d42bfd102357082c8d17ac743c10ae3dc88a81b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 15:00:03 -0300

    Saved julia/main.py at Timestamp: 2018-08-08 18:00:03

--- julia/main.py
+++ julia/main.py
@@ -1,2 +1,2 @@
 from _spy.vitallino.main import Cena ,Elemento
-CasaDoPennisWise(img(img = http://mouse.latercera.com/wp-content/uploads/2017/09/it-casa.jpg)
+CasaDoPennisWise(img = http://mouse.latercera.com/wp-content/uploads/2017/09/it-casa.jpg)
