50eeb7d4be1e67dc9b162a258ea760b8055959f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 15:48:25 -0300

    Created naomi/main.py at Timestamp: 2018-08-08 18:48:24

---
+++ naomi/main.py
@@ -0,0 +1,5 @@
+from _ spy.vitollino.main import Cena
+cenaDoTerror = Cena (img="https://www.recantodasletras.com.br/img/?id=1189395&maxw=495&maxh=660")
+cenaDoTerror.vai()
+personagem1=Elemento(img=link_personagem1,sTyle=dict(top=9,left ='8px',width=6))
+pensongem1.entra(cena1)
