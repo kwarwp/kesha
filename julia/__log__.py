607d246fef59bc6675b9fc411ee1e21aebe77299
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 15:10:18 -0300

    Saved julia/__log__.py at Timestamp: 2018-08-08 18:10:18

--- julia/__log__.py
+++ julia/__log__.py
@@ -62,3 +62,19 @@ SyntaxError: invalid syntax
                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 08 2018 15:11:02.316 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    casaDoPennisWise= Cena(img ="http://mouse.latercera.com/wp-content/uploads/2017/09/it-casa.jpg")
+TypeError: 'module' object is not callable
+'''},
