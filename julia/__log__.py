488f893c2a4a8e9c62e893773080ca51c5feff5b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 14:29:19 -0300

    Saved julia/__log__.py at Timestamp: 2018-08-08 17:29:19

--- julia/__log__.py
+++ julia/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 08 2018 14:29:56.37 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  https://www.google.com.br/
+        ^
+SyntaxError: invalid syntax
+'''},
