206833097cfbb51eb03072b9d0a308f3fdbb21f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 15:02:48 -0300

    Created naomi/__log__.py at Timestamp: 2018-08-08 18:02:48

---
+++ naomi/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Wed Aug 08 2018 15:08:12.157 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    cenadoterror.vai()
+NameError: name 'cenadoterror' is not defined
+'''},
