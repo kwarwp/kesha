bfcdeccf77970038fa48ea789ad53b523ca97532
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 15:46:45 -0300

    Saved naomi/__log__.py at Timestamp: 2018-08-08 18:46:45

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -15,3 +15,19 @@
     cenadoterror.vai()
 NameError: name 'cenadoterror' is not defined
 '''},
+{'date': 'Wed Aug 08 2018 15:52:11.483 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    personagem1=Elemento(img=link_personagem1,sTyle=dict(top=9,left ='8px',width=6))
+NameError: name 'Elemento' is not defined
+'''},
