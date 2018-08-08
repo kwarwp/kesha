e294bd3fad3dddb1ec77776b1f2a444938920e28
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 15:59:24 -0300

    Saved julia/main.py at Timestamp: 2018-08-08 18:59:24

--- julia/main.py
+++ julia/main.py
@@ -1,2 +1,6 @@
-from _spy.vitallino.main import Cena ,Elemento
-CasaDoPennisWise(img = http://mouse.latercera.com/wp-content/uploads/2017/09/it-casa.jpg)
+from _spy.vitollino.main import Cena , Elemento
+ghostcity = Cena(img = "http://4.bp.blogspot.com/-8Iiif5ygd8k/VImL62UD2II/AAAAAAAAHvs/YeBNNJFW1EY/s1600/271517.jpg")
+death = Elemento(img = "http://www.planetmoviestore.com.br/7267-thickbox_default/pre-venda-t800-bd-terminator-14-exterminador-do-futuro-.jpg"
+, style=dict(top=150,left=100,height = "100px",width =100))
+death.entra(ghostcity)
+ghostcity.vai()
