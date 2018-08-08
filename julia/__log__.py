0fac748b7ba58e49b908d5b36b765c0d26b58cca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 14:25:14 -0300

    Created julia/__log__.py at Timestamp: 2018-08-08 17:25:14

---
+++ julia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 08 2018 14:25:53.732 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  cena1 = Cena(img = https://www.urbanarts.com.br/imagens/produtos/026863/0/Ampliada/paisagem.jpg)
+                           ^
+SyntaxError: invalid syntax
+'''},
