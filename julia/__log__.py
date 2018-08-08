137123e150b25ddb3e3a4a3d98fa6efb5015090b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 14:59:41 -0300

    Saved julia/__log__.py at Timestamp: 2018-08-08 17:59:40

--- julia/__log__.py
+++ julia/__log__.py
@@ -41,3 +41,10 @@ SyntaxError: invalid syntax
       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 08 2018 15:00:24.990 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  CasaDoPennisWise(img(img = http://mouse.latercera.com/wp-content/uploads/2017/09/it-casa.jpg)
+                                  ^
+SyntaxError: invalid syntax
+'''},
