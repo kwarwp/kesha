1622d189b46212be2700831219ff3e46c388edbf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-08 15:24:32 -0300

    Saved julia/__log__.py at Timestamp: 2018-08-08 18:24:32

--- julia/__log__.py
+++ julia/__log__.py
@@ -124,3 +124,19 @@ SyntaxError: invalid syntax
     voucomerteucu = Cena(img = "https://www.sunhome.ru/i/wallpapers/212/oboi-rabochego-stola-uzhasi.orig.jpg")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Wed Aug 08 2018 16:29:48.505 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    death
+NameError: name 'death' is not defined
+'''},
