f4fbbf37a40b7dcec4f6d209192a07ec4219caaa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-11 20:08:58 -0300

    Saved parisa/main.py at Timestamp: 2018-08-11 23:08:58

--- parisa/main.py
+++ parisa/main.py
@@ -39,5 +39,9 @@ parede_area1g= box(pos=(-2,-0.9,1.35), size=(4,0.2,2.5), color=color.white)
 parede_area1h= box(pos=(2.9,-2.9,1.35), size=(6,0.2,2.5), color=color.white, opacity=0.3)
 parede_area1i= box(pos=(0,-1.8,1.35), size=(0.2,2,2.5), color=color.white)
 
-parede_area1j= box(pos=(0,1,1.35), size=(4.2,0.1,2.5), color=color.white, opacity=0.5)
+parede_area1j= box(pos=(-0.2,1,1.35), size=(4.3,0.1,2.5), color=color.white, opacity=0.5)
+parede_area1k= box(pos=(1.9,0,1.35), size=(0.1,2,2.5), color=color.white)
+parede_area1l= box(pos=(1.9,-2,1.35), size=(0.1,2,2.5), color=color.white, opacity=0.5)
+
+
 
