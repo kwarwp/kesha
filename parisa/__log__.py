71d36bb86a1cb69eaf40e21cf1259f937596d767
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-11 18:11:38 -0300

    Saved parisa/__log__.py at Timestamp: 2018-08-11 21:11:38

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -15,3 +15,19 @@
     area1 = box(pos=(-5,0,0), size=(2,6,0.2), color= blue)
 NameError: name 'blue' is not defined
 '''},
+{'date': 'Sat Aug 11 2018 18:11:34.838 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    area4= box(pos=(3,1,0), size=(2,4,0.2), color= color.brown)
+AttributeError: no attribute brown for [object Object]
+'''},
