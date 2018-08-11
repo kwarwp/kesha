e9da0d3ba9102fdc5222074b49710e9ad0a78ec6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-11 17:45:02 -0300

    Saved parisa/main.py at Timestamp: 2018-08-11 20:45:02

--- parisa/main.py
+++ parisa/main.py
@@ -1,5 +1,4 @@
 # kesha.parisa.main.py
-# kesha.libby.main.py
 from browser import doc, html
 #doc["glowscript"] <= html.H1('XXXXXXXXX -------------- XXXXXXXXXXX')
 from _spy.vpython.main import *
@@ -11,9 +10,12 @@ scene.width = 800
 scene.height = 600
 bloco = dict(color=color.blue)
 
-simetria1=box(pos=(4.2, -2, 0), size=(1,1,1) , **bloco)
-simetria2=box(pos=(1.8, 1.6, 0), size=(1,1,1) , **bloco)
-simetria = [box(pos=(3, x*(1.2)-2, 0), size=(1,1,1), **bloco) for x in range(4)]
 
-pts = [simetria1, simetria2]+simetria
-sup = compound(pts, pos=vec(2,0,0), axis=vec(4,0,-1))
+#simetria1=box(pos=(4.2, -2, 0), size=(1,1,1) , **bloco)
+#simetria2=box(pos=(1.8, 1.6, 0), size=(1,1,1) , **bloco)
+#simetria = [box(pos=(3, x*(1.2)-2, 0), size=(1,1,1), **bloco) for x in range(4)]
+
+#pts = [simetria1, simetria2]+simetria
+#sup = compound(pts, pos=vec(2,0,0), axis=vec(4,0,-1))
+
+area1 = box(pos=(2,2,2), size=(2,6,0.2))
