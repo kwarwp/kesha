afba02e61e4021cf8891e8cd64654fc05a226db0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-11 21:17:47 -0300

    Saved parisa/__log__.py at Timestamp: 2018-08-12 00:17:47

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -95,3 +95,19 @@ AttributeError: no attribute grey for [object Object]
     label(pos=(60,120,80), text='MUSEU DA GEODIVERSIDADE')
 NameError: name 'label' is not defined
 '''},
+{'date': 'Sat Aug 11 2018 21:17:44.837 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    label(pos=(60,120,80), text='MUSEU DA GEODIVERSIDADE')
+NameError: name 'label' is not defined
+'''},
