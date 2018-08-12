dde0e72d4380e8ed586be3a8b2158aa4d82ebaa2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 20:17:19 -0300

    Saved parisa/__log__.py at Timestamp: 2018-08-12 23:17:19

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -231,3 +231,10 @@ AttributeError: no attribute grey for [object Object]
     area8= box(pos=(1,-2.5,0), size=(2,1,0.2), color=color.grey)
 AttributeError: no attribute grey for [object Object]
 '''},
+{'date': 'Sun Aug 12 2018 20:17:17.964 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 22
+  area9= box(pos=(4,-2,0), size=(4,2,0.2), color=color.white))
+                                                             ^
+SyntaxError: invalid syntax
+'''},
