875f219493019c5102f988e316cee488b8136209
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 20:26:50 -0300

    Saved parisa/main.py at Timestamp: 2018-08-12 23:26:50

--- parisa/main.py
+++ parisa/main.py
@@ -19,11 +19,11 @@ area5= box(pos=(-2,2,0), size=(4,2,0.2), color=color.green)
 area6= box(pos=(1,2,0), size=(2,2,0.2), color=color.red)
 area7= box(pos=(1,-1.5,0), size=(2,1,0.2), color=color.orange)
 area8= box(pos=(1,-2.5,0), size=(2,1,0.2), color=color.grey)
-area9= box(pos=(4,-2,0), size=(4,2,0.2), color=color.grey)
-#area9= box(pos=(4,-2,0), size=(4,2,0.2), color=color.blue))
+area9= box(pos=(4,-2,0), size=(4,2,0.2), color=color.blue)
+
 
 parede_a= box(pos=(-5,-2.9,1.35), size=(2,0.2,2.5), color=color.white)
-parede_b= box(pos=(-5.9,0,1.35), size=(0.2,6,2.5), color=color.white)
+"""parede_b= box(pos=(-5.9,0,1.35), size=(0.2,6,2.5), color=color.white)
 parede_c= box(pos=(-1,2.9,1.35), size=(10,0.2,2.5), color=color.white)
 parede_d= box(pos=(-4.1,-1.9,1.35), size=(0.2,2.2,2.5), color=color.white, opacity=0.3)
 parede_e= box(pos=(-4.1,1.9,1.35), size=(0.2,2.2,2.5), color=color.white, opacity=0.3)
@@ -35,3 +35,4 @@ parede_j= box(pos=(-0.2,1,1.35), size=(4.3,0.1,2.5), color=color.white, opacity=
 parede_k= box(pos=(1.9,0,1.35), size=(0.1,2,2.5), color=color.white)
 parede_l= box(pos=(1.9,-2,1.35), size=(0.1,2,2.5), color=color.white, opacity=0.3)
 parede_m= box(pos=(1,-2,1.35), size=(2,0.1,2.5), color=color.white, opacity=0.3)
+"""
