631b21d56b9d69e663ca2f34eb9e381a66010859
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 09:28:03 -0300

    Saved parisa/__log__.py at Timestamp: 2018-08-12 12:28:03

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -155,3 +155,19 @@ NameError: name 'label' is not defined
 'error': '''
 Exception: <Error: Font not available for text object.>
 '''},
+{'date': 'Sun Aug 12 2018 09:27:59.567 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    area8= box(pos=(1,-2.5,0), size=(2,1,0.2), color=color.grey)
+AttributeError: no attribute grey for [object Object]
+'''},
