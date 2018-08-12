449ef46d1336d2c68d524a9033f48ee997781fcc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-12 20:11:53 -0300

    Saved parisa/main.py at Timestamp: 2018-08-12 23:11:53

--- parisa/main.py
+++ parisa/main.py
@@ -5,17 +5,11 @@ from _spy.vpython.main import *
 doc['pydiv'].html=''
 _gs=Glow('pydiv')
 scene=canvas()
-scene.background= color.black #"rgba(0, 0, 200, 0.5)"
+scene.background= color.black 
 scene.width = 1200
 scene.height = 600
 
-t1 = text(text='MUSEU DA GEODIVERSIDADE', pos=(-5,3,0), depth=0.5, color=color.yellow)
-#bloco = dict(color=color.blue)
-#simetria1=box(pos=(4.2, -2, 0), size=(1,1,1) , **bloco)
-#simetria2=box(pos=(1.8, 1.6, 0), size=(1,1,1) , **bloco)
-#simetria = [box(pos=(3, x*(1.2)-2, 0), size=(1,1,1), **bloco) for x in range(4)]
-#pts = [simetria1, simetria2]+simetria
-#sup = compound(pts, pos=vec(2,0,0), axis=vec(4,0,-1))
+#t1 = text(text='MUSEU DA GEODIVERSIDADE', pos=(-5,3,0), depth=0.5, color=color.yellow)
 
 area1 = box(pos=(-5,0,0), size=(2,6,0.2), color=color.blue)
 area2= box(pos=(-3,0,0), size=(2,2,0.2), color=color.yellow)
@@ -26,3 +20,19 @@ area6= box(pos=(1,2,0), size=(2,2,0.2), color=color.red)
 area7= box(pos=(1,-1.5,0), size=(2,1,0.2), color=color.orange)
 area8= box(pos=(1,-2.5,0), size=(2,1,0.2), color=color.grey)
 area9= box(pos=(4,-2,0), size=(4,2,0.2))
+
+parede_a= box(pos=(-5,-2.9,1.35), size=(2,0.2,2.5), color=color.white)
+parede_b= box(pos=(-5.9,0,1.35), size=(0.2,6,2.5), color=color.white)
+parede_c= box(pos=(-1,2.9,1.35), size=(10,0.2,2.5), color=color.white)
+parede_d= box(pos=(-4.1,-1.9,1.35), size=(0.2,2.2,2.5), color=color.white, opacity=0.3)
+parede_e= box(pos=(-4.1,1.9,1.35), size=(0.2,2.2,2.5), color=color.white, opacity=0.3)
+
+parede_f= box(pos=(3.9,1,1.35), size=(0.2,4,2.5), color=color.white)
+parede_g= box(pos=(-2,-0.9,1.35), size=(4,0.2,2.5), color=color.white)
+parede_h= box(pos=(2.9,-2.9,1.35), size=(6,0.2,2.5), color=color.white, opacity=0.3)
+parede_i= box(pos=(0,-1.8,1.35), size=(0.2,2,2.5), color=color.white)
+
+parede_j= box(pos=(-0.2,1,1.35), size=(4.3,0.1,2.5), color=color.white, opacity=0.5)
+parede_k= box(pos=(1.9,0,1.35), size=(0.1,2,2.5), color=color.white)
+parede_l= box(pos=(1.9,-2,1.35), size=(0.1,2,2.5), color=color.white, opacity=0.3)
+parede_m= box(pos=(1,-2,1.35), size=(2,0.1,2.5), color=color.white, opacity=0.3)
