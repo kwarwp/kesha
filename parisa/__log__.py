3e935bbbff4eb96290003090744312f1839208a7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-11 21:17:40 -0300

    Saved parisa/__log__.py at Timestamp: 2018-08-12 00:17:40

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -79,3 +79,19 @@ AttributeError: no attribute brown for [object Object]
     area8= box(pos=(1,-2.5,0), size=(2,1,0.2), color=color.grey)
 AttributeError: no attribute grey for [object Object]
 '''},
+{'date': 'Sat Aug 11 2018 21:17:38.78 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    label(pos=(60,120,80), text='MUSEU DA GEODIVERSIDADE')
+NameError: name 'label' is not defined
+'''},
