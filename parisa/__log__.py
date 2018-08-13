7323b1ba8b1a7bd7e36a42d8f08f605b77160732
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-13 10:41:44 -0300

    Saved parisa/__log__.py at Timestamp: 2018-08-13 13:41:44

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -369,3 +369,21 @@ SyntaxError: invalid syntax
                                                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Aug 13 2018 10:37:41.300 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    parede_f.rotate(angle=90, axis=(1,0,0)) 
+  module _spy.vpython.primitive line 23
+    kwargs['axis'] = kwargs['axis']._vec
+AttributeError: 'tuple' object has no attribute '_vec'
+'''},
