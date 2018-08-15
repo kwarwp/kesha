1c6d23020d4df0264957ce202b8feb8205a78748
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-15 14:07:42 -0300

    Saved parisa/__log__.py at Timestamp: 2018-08-15 17:07:42

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -423,3 +423,21 @@ AttributeError: 'tuple' object has no attribute '_vec'
     kwargs['axis'] = kwargs['axis']._vec
 AttributeError: 'tuple' object has no attribute '_vec'
 '''},
+{'date': 'Wed Aug 15 2018 14:07:23.458 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    parede_f.rotate(angle=pi, axis=(1,0,0)) 
+  module _spy.vpython.primitive line 23
+    kwargs['axis'] = kwargs['axis']._vec
+AttributeError: 'tuple' object has no attribute '_vec'
+'''},
