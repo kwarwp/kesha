db30bcedc81b4243d1cd4d5738779d7ea52180fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-22 17:00:16 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-22 20:00:16

--- angie/__log__.py
+++ angie/__log__.py
@@ -27,3 +27,10 @@ SyntaxError: invalid syntax
    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 22 2018 17:00:17.146 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  <head>
+   ^
+SyntaxError: invalid syntax
+'''},
