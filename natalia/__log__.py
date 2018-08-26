a6e6bb12e99d630a39edbbef6af6c3553126fa4d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-26 13:54:08 -0300

    Created natalia/__log__.py at Timestamp: 2018-08-26 16:54:08

---
+++ natalia/__log__.py
@@ -0,0 +1,5 @@
+
+{'date': 'Sun Aug 26 2018 13:54:00.897 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <TypeError: Cannot read property 'charAt' of undefined>
+'''},
