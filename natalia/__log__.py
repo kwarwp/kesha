107a365e90d4ed4bd9de8e8641e8eff072576e4a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-26 14:04:42 -0300

    Saved natalia/__log__.py at Timestamp: 2018-08-26 17:04:42

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -27,3 +27,19 @@ Exception: <TypeError: Cannot read property 'charAt' of undefined>
     Sala3D(IMGS[1], p=(4, -4.1), width=100, height="80px")
 TypeError: __init__() got an unexpected keyword argument 'width'
 '''},
+{'date': 'Sun Aug 26 2018 14:04:35.172 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 99
+    Sala3D(IMGS[1], p=(4, -4.1), width=100, height="80px")
+TypeError: __init__() got an unexpected keyword argument 'width'
+'''},
