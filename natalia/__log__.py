930dd443d6e04dc3962ad6d445a4fd4f5abe010b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-26 15:43:40 -0300

    Saved natalia/__log__.py at Timestamp: 2018-08-26 18:43:39

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -208,3 +208,19 @@ NameError: name 'A' is not defined
     Sala3D(IMGS[E], p=(-8,-4))
 NameError: name 'E' is not defined
 '''},
+{'date': 'Sun Aug 26 2018 15:43:33.16 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 108
+    Sala3D(IMGS[E], p=(-8,-4))
+NameError: name 'E' is not defined
+'''},
