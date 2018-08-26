e95d89a56451af91fd94f2a39d9d050fc5f5f90c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-26 15:45:37 -0300

    Saved natalia/__log__.py at Timestamp: 2018-08-26 18:45:37

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -224,3 +224,19 @@ NameError: name 'E' is not defined
     Sala3D(IMGS[E], p=(-8,-4))
 NameError: name 'E' is not defined
 '''},
+{'date': 'Sun Aug 26 2018 15:45:30.138 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    IMGS = [[MUSEU["C{:01X}{}".format(sala, rosa)] for rosa in ROSA] for sala in range(15)]
+KeyError: CA_NORTE
+'''},
