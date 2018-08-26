12b2f9b0a48881db397445fe1790d20ddb139eee
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-26 15:11:29 -0300

    Saved natalia/__log__.py at Timestamp: 2018-08-26 18:11:29

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -160,3 +160,19 @@ NameError: name 'A' is not defined
     IMGS = [[MUSEU["C{:01X}{}".format(sala, rosa)] for rosa in ROSA] for sala in range(15)]
 KeyError: CA_NORTE
 '''},
+{'date': 'Sun Aug 26 2018 15:11:22.427 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    IMGS = [[MUSEU["C{:01X}{}".format(sala, rosa)] for rosa in ROSA] for sala in range(15)]
+KeyError: CA_NORTE
+'''},
