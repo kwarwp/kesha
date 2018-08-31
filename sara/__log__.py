9183573ff91cc658fb3c823a013a92827d074603
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-31 15:41:22 -0300

    Saved sara/__log__.py at Timestamp: 2018-08-31 18:41:22

--- sara/__log__.py
+++ sara/__log__.py
@@ -169,3 +169,19 @@ IndentationError: unexpected indent
     STYLE["width"]=800
 AttributeError: 'module' object has no attribute '__setitem__'
 '''},
+{'date': 'Fri Aug 31 2018 16:47:25.215 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 1
+    rb2d = GetComponent<Rigidbody2D> ();
+NameError: name 'Rigidbody2D' is not defined
+'''},
