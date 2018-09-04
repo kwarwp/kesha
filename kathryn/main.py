df5e5d5f90770758235f91803da12ff95276669f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-04 20:11:43 -0300

    Created kathryn/main.py at Timestamp: 2018-09-04 23:11:43

---
+++ kathryn/main.py
@@ -0,0 +1,25 @@
+# kesha.libby.main.py
+from browser import doc, html
+#doc["glowscript"] <= html.H1('XXXXXXXXX -------------- XXXXXXXXXXX')
+from _spy.vpython.main import *
+doc['pydiv'].html=''
+_gs=Glow('pydiv')
+scene=canvas()
+scene.background= color.black #"rgba(0, 0, 200, 0.5)"
+scene.width = 1200
+scene.height = 600
+bloco = dict(color=color.blue)
+
+cubo=box(pos=(2.2, -2, 0), size=(2,2,2) , **bloco)
+#simetria2=box(pos=(1.8, 1.6, 0), size=(1,1,1) , **bloco)
+#simetria = [box(pos=(3, x*(1.2)-2, 0), size=(1,1,1), **bloco) for x in range(4)]
+
+#pts = [simetria1, simetria2]+simetria
+#sup = compound(pts, pos=vec(2,0,0), axis=vec(4,0,-1))
+
+#def sail():
+    #rate(2, sail)
+    #sup.pos += vec(0.2,0,0)
+#   sup.rotate(angle=0.02, axis=vec(1,1,0))
+
+#sail()
