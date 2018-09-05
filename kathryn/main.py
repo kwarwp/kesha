053ee60d85d6a77c2da1c0162e9cbfc080a297c1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-04 21:09:43 -0300

    Saved kathryn/main.py at Timestamp: 2018-09-05 00:09:43

--- kathryn/main.py
+++ kathryn/main.py
@@ -7,7 +7,7 @@ scene=canvas()
 scene.background= color.black #"rgba(0, 0, 200, 0.5)"
 scene.width = 1200
 scene.height = 800
-bloco = dict(color=color.white)
+bloco = dict(color=color.blue)
 bloco1 = dict(color=color.white)
 
 cubo1=box(pos=(1, -1.2, 0), size=(1,1,1) , **bloco)
