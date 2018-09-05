d658aac656e693d6d02e58f50789b77eba28d9b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-05 10:48:05 -0300

    Saved natalia/main.py at Timestamp: 2018-09-05 13:48:05

--- natalia/main.py
+++ natalia/main.py
@@ -85,7 +85,7 @@ class Sala3D:
 class Sala3Dgrande:
     def __init__(self, img_list, p=(0,0)):
         for direcao, parede in enumerate(img_list):
-            parede_ = box(pos=(2*POS[direcao][0]+p[0], 0, -2*POS[direcao][1]+p[1]), size=(0.2, 8, 8), texture=dict(file=parede, place=["right"]))
+            parede_ = box(pos=(4*POS[direcao][0]+p[0], 0, -4*POS[direcao][1]+p[1]), size=(0.2, 4, 8), texture=dict(file=parede, place=["right"]))
             
             parede_.rotate(angle=direcao*pi/2.0, axis=vec(0,-1,0))
 """
