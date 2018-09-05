b2a2055188c3a6d61369c8b4e9882a886c3abd5a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-04 21:38:59 -0300

    Saved callie/__log__.py at Timestamp: 2018-09-05 00:38:58

--- callie/__log__.py
+++ callie/__log__.py
@@ -43,3 +43,31 @@ NameError: name 'offset' is not defined
     BUTTONS = [TENTAR, DESISTO, NAOQUEROJOGAR, TERMINEI, JATERMINEI]
 NameError: name 'JATERMINEI' is not defined
 '''},
+{'date': 'Tue Sep 04 2018 21:38:48.633 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 205
+    Jogo()
+  module <module> line 194
+    self.reset()
+  module <module> line 197
+    self.tabuleiro =  Tabuleiro()
+  module <module> line 77
+    self.inicia_fase()
+  module <module> line 81
+    [self.cria_tabuleiro(col=3, lin=self.linha, lado=l) for l in self.lado]
+  module <module> line 120
+    self.casa["esquerda"][nome] = Casa(self, nome=nome, linha=linha_, coluna=coluna_, lado=lado)
+  module <module> line 171
+    inicio_x_ = inicio_x + offset[lado]
+NameError: name 'offset' is not defined
+'''},
