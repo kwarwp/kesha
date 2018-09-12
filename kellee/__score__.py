edecfdb58d2502067d83734aa3d6742cc4f02e79
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-12 09:54:57 -0300

    Saved kellee/__score__.py at Timestamp: 2018-09-12 12:54:57

--- kellee/__score__.py
+++ kellee/__score__.py
@@ -1065,3 +1065,4 @@
       {'doc_id': '53ccfe1d', 'carta': 'carta_10', 'casa': 'esquerda_0_1_e', 'move': 'MOVE', 'ponto': 10, 'valor': 'N', '_level': 3, 'tempo': 1536756890389},
       {'doc_id': '53ccfe1d', 'carta': 'carta_9', 'casa': 'esquerda_1_1_e', 'move': 'MOVE', 'ponto': 10, 'valor': 'N', '_level': 3, 'tempo': 1536756892663},
       {'doc_id': '53ccfe1d', 'carta': 'carta_8', 'casa': 'esquerda_2_1_e', 'move': 'MOVE', 'ponto': 10, 'valor': 'N', '_level': 3, 'tempo': 1536756894733},
+      {'doc_id': '53ccfe1d', 'carta': 'carta_7', 'casa': 'esquerda_3_1_e', 'move': 'MOVE', 'ponto': 10, 'valor': 'N', '_level': 3, 'tempo': 1536756896482},
