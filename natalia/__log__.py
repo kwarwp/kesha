75ddbe7ddff5745aa6e77eb6516b0c63ae6b4e71
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-11 23:45:59 -0300

    Saved natalia/__log__.py at Timestamp: 2018-09-12 02:45:59

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -308,3 +308,21 @@ NameError: name 'POS1' is not defined
     parede_.rotate(angle=direcao*pi/2.0, axis=vec(0,-1,0))
 UnboundLocalError: local variable 'i' referenced before assignment
 '''},
+{'date': 'Tue Sep 11 2018 23:45:58.449 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 115
+    Sala3Dlongo(IMGS[0], p=(4,0))
+  module <module> line 100
+    parede_.rotate(angle=direcao*pi/2.0, axis=vec(0,-1,0))
+UnboundLocalError: local variable 'i' referenced before assignment
+'''},
