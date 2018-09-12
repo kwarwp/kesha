97bf93c4694cbb94a468f69a56ea35e5b62bac8d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-12 14:10:35 -0300

    Saved natalia/__log__.py at Timestamp: 2018-09-12 17:10:35

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -333,3 +333,21 @@ UnboundLocalError: local variable 'i' referenced before assignment
                                 ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Wed Sep 12 2018 14:10:36.326 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 115
+    Sala3Dlongo(IMGS[0], p=(6,0))
+  module <module> line 95
+    if i%2 == 0:
+NameError: name 'i' is not defined
+'''},
