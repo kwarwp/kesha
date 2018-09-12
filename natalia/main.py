1cdcc96210ede092d9be9f673c00e48ef5d6d239
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-12 14:24:23 -0300

    Saved natalia/main.py at Timestamp: 2018-09-12 17:24:23

--- natalia/main.py
+++ natalia/main.py
@@ -76,7 +76,7 @@ scene = canvas()
 POS=[(-1,0),(0,1),(1,0),(0,-1)]
 
 class Sala3D:
-    def __init__(self, img_list, p=(0,0,0)):
+    def __init__(self, img_list, p=(0,0)):
         for direcao, parede in enumerate(img_list):
             parede_ = box(pos=(2*POS[direcao][0]+p[0], 0, -2*POS[direcao][1]+p[1]), size=(0.2, 4, 4), texture=dict(file=parede, place=["right"]))
             
@@ -113,7 +113,7 @@ class Museu:
 # Sala3D(IMG_LIST1, p=(4, 0))
 
 #SALA 0
-Sala3Dlongo(IMGS[0], p=(6,0,1))
+Sala3Dlongo(IMGS[0], p=(8,8,1))
 #Sala3D(IMGS[0], p=(8,0))
 
 #SALA 1
