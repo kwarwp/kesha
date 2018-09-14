734daf3c288a3be0a00af99c6050448cb52ffcd6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-14 10:40:24 -0300

    Saved natalia/__log__.py at Timestamp: 2018-09-14 13:40:23

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -524,3 +524,19 @@ TypeError: canvas() got an unexpected keyword argument 'left'
     scene = canvas(display(left=100,top="10px", width=1000,height="800px"))
 NameError: name 'display' is not defined
 '''},
+{'date': 'Fri Sep 14 2018 10:40:21.281 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    scene = canvas(display(left=100,top="10px", width=1000,height="800px"))
+NameError: name 'display' is not defined
+'''},
