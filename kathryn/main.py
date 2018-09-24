4d15cd7f9fca9149f93b8fca9b2b9e6344f0bcd7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 15:23:08 -0300

    Saved kathryn/main.py at Timestamp: 2018-09-24 18:23:08

--- kathryn/main.py
+++ kathryn/main.py
@@ -10,12 +10,6 @@ scene.height = 800
 bloco = dict(color=color.blue)
 bloco1 = dict(color=color.white)
 
-
-
-
-
-
-
 #cubo4=box(pos=(-0.2, 1.2,  0.0), size=(1,1,1) , **bloco)
 #cubo5=box(pos=(-0.2, 1.2,  1.2), size=(1,1,1) , **bloco)
 
@@ -32,11 +26,10 @@ bloco1 = dict(color=color.white)
 #cubo8=box(pos=(-1.2,-2.4, -1.2), size=(1,1,1) , **bloco1)
 #cubo9=box(pos=(-1.2,-3.6, -1.2), size=(1,1,1) , **bloco1)
 
-#cubos1 = [box(pos = (-0.2, 1.2, x * (1.2)),size(1, 1, 1),**bloco1)for x in range(2)]  
-cubos1 = [box(pos=(-0.2, 1.2, x * (1.2) - 0), size=(1, 1, 1), **bloco) for x in range(2)] #Os da primeira em cima
+cubos1 = [box(pos=(-0.2, 1.2, x * (1.2) - 0), size=(1, 1, 1), **bloco1) for x in range(2)] #Os da primeira em cima
 cubos2 = [box(pos=(y * (1.2) - 1.4, 0, 0),size=(1, 1, 1),**bloco)for y in range(3)] #Os da segunda linha
-cubo = box(pos=(-0.2,   0, -1.2), size=(1,1,1) , **bloco1) #Antigo Cubo 5 na 2 linha
-cubo2 = box(pos=(1.0, -1.2, 0.0), size=(1,1,1) , **bloco) #Antigo Cubo 1 na 3 linha
+cubo = box(pos=(-0.2,   0, -1.2), size=(1,1,1) , **bloco) #Antigo Cubo 5 na 2 linha
+cubo2 = box(pos=(1.0, -1.2, 0.0), size=(1,1,1) , **bloco1) #Antigo Cubo 1 na 3 linha
 cubo3 = box(pos=(-0.2,-2.4, 1.2), size=(1,1,1) , **bloco) #Antigo Cubo 7 na 4 linha
 cubos3 = [box(pos=(-1.2,(-1.2) * z - 2.4, -1.2),size=(1, 1, 1),**bloco1)for z in range(2)]  # Os últimos
 
