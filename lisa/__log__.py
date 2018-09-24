c276195b3735558ed92ac97a43ea65371bf883b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 13:59:59 -0300

    Saved lisa/__log__.py at Timestamp: 2018-09-24 16:59:59

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -35,3 +35,23 @@ NameError: name 'cena' is not defined
     proximafase3 = Tabuleiro(**FASE3)
 TypeError: __init__() got an unexpected keyword argument '_3d'
 '''},
+{'date': 'Mon Sep 24 2018 13:59:54.218 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 211
+    Jogo()
+  module <module> line 200
+    self.reset()
+  module <module> line 208
+    proximafase3 = Tabuleiro(**FASE3)
+TypeError: __init__() got an unexpected keyword argument '_3d'
+'''},
