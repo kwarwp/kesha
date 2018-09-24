3f035bfcaae9fb27286b7220e742f82b3c13bca7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 14:23:54 -0300

    Saved kellee/__log__.py at Timestamp: 2018-09-24 17:23:53

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -778,3 +778,31 @@ NameError: name 'offset' is not defined
                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 24 2018 14:23:50.60 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 209
+    Jogo()
+  module <module> line 198
+    self.reset()
+  module <module> line 201
+    self.tabuleiro =  Tabuleiro()
+  module <module> line 82
+    self.inicia_fase()
+  module <module> line 88
+    Carta(self, "carta_{}".format(i))
+  module <module> line 141
+    self.cria_carta(carta_id)
+  module <module> line 146
+    self.tabuleiro.empilha(self)
+AttributeError: 'Tabuleiro' object has no attribute 'empilha'
+'''},
