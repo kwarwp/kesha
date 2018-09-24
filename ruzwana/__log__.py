a89131495dad2681e99d789fa46f8060b977abbc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 17:31:06 -0300

    Created ruzwana/__log__.py at Timestamp: 2018-09-24 20:31:06

---
+++ ruzwana/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 24 2018 17:31:03.431 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  list1 = [(0,1.2),(0,-1.2),(-1.2,0),(1.2,0)
+                                              ^
+SyntaxError: invalid syntax
+'''},
