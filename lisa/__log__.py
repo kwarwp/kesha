21c704f5afbec2b6d8dae1b9637e7a868f2f03c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 14:32:10 -0300

    Saved lisa/__log__.py at Timestamp: 2018-09-24 17:32:09

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -75,3 +75,31 @@ TypeError: __init__() got an unexpected keyword argument '_3d'
     proximafase3 = Tabuleiro(**FASE3)
 TypeError: __init__() got an unexpected keyword argument '_3d'
 '''},
+{'date': 'Mon Sep 24 2018 14:32:08.325 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 217
+    Jogo()
+  module <module> line 204
+    self.reset()
+  module <module> line 207
+    self.tabuleiro =  Tabuleiro()
+  module <module> line 87
+    self.inicia_fase()
+  module <module> line 91
+    [self.cria_tabuleiro(col=3, lin=self.linha, lado=l) for l in self.lado]
+  module <module> line 130
+    self.casa["esquerda"][nome] = Casa(self, nome=nome, linha=linha_, coluna=coluna_, lado=lado)
+  module <module> line 182
+    self.nome = nome_elemento = "{}_{}".format(tabuleiro.nome, nome)
+AttributeError: 'Tabuleiro' object has no attribute 'nome'
+'''},
