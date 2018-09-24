13afbae80d62844c0aaa535c6fe101f403f3f652
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 15:11:00 -0300

    Saved kathryn/__log__.py at Timestamp: 2018-09-24 18:10:59

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -38,3 +38,10 @@ TypeError: box() got multiple values for argument 'size'
                                                           ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Mon Sep 24 2018 15:10:59.355 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  cubos1 = [box(pos = (-0.2, 1.2, 0.0 + 1.2*x),size(1, 1, 1),**bloco1)for x in range(2)]  # Os da primeira em cima
+                                                            ^
+SyntaxError: non-keyword arg after keyword arg
+'''},
