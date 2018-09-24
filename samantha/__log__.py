4685ca6e87e9a23bb0b732f0e4bc2a8f2b624f4c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 00:00:50 -0300

    Saved samantha/__log__.py at Timestamp: 2018-09-24 03:00:49

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -832,3 +832,21 @@ Exception: <TypeError: Cannot read property 'canvas_selected' of undefined>
     doc['py3d'].html = ''
 KeyError: py3d
 '''},
+{'date': 'Mon Sep 24 2018 00:00:51.468 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    Tabuleiro()
+  module <module> line 39
+    elem = Elemento(button, cena=self.elemento, vai=self.buttonapertado,tit="button_{}_".format(i), style=dict(
+AttributeError: 'Tabuleiro' object has no attribute 'buttonapertado'
+'''},
