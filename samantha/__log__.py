95f22c27dd1039f12f871b6a6ef182a15810c833
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-23 23:48:46 -0300

    Saved samantha/__log__.py at Timestamp: 2018-09-24 02:48:46

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -796,3 +796,19 @@ Exception: self.$events is undefined
   module '$exec_7881' line 115
 Tabuleiro()
 '''},
+{'date': 'Sun Sep 23 2018 23:48:47.832 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 18
+    simetria1 = box(pos=(4.2, -2, 0), size=(1, 1, 1), **bloco)
+NameError: name 'bloco' is not defined
+'''},
