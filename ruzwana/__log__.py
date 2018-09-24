a55b666eb88ae31abf253e07f5480f12a6ee6314
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 18:19:06 -0300

    Saved ruzwana/__log__.py at Timestamp: 2018-09-24 21:19:06

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -153,3 +153,19 @@ TypeError: rate() missing 1 positional argument: ['func']
     rate(1/dt)
 TypeError: rate() missing 1 positional argument: ['func']
 '''},
+{'date': 'Mon Sep 24 2018 18:19:05.291 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    rate(1/dt)
+TypeError: rate() missing 1 positional argument: ['func']
+'''},
