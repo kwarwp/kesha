129bfa118875c8fa05fc39f75168813e402b301d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 14:32:57 -0300

    Saved lisa/__log__.py at Timestamp: 2018-09-24 17:32:57

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -103,3 +103,25 @@ TypeError: __init__() got an unexpected keyword argument '_3d'
     self.nome = nome_elemento = "{}_{}".format(tabuleiro.nome, nome)
 AttributeError: 'Tabuleiro' object has no attribute 'nome'
 '''},
+{'date': 'Mon Sep 24 2018 14:32:51.543 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 217
+    Jogo()
+  module <module> line 204
+    self.reset()
+  module <module> line 216
+    self.tabuleiro.vai()
+  module <module> line 111
+    print(self._3d, len(self.pilha_de_cartas))
+AttributeError: 'Tabuleiro' object has no attribute '_3d'
+'''},
