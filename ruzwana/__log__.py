3f2eb7386ba12efead74c1c9fa4c27a8f7d72001
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 17:31:54 -0300

    Saved ruzwana/__log__.py at Timestamp: 2018-09-24 20:31:53

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 24 2018 17:31:52.980 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  cubos1 = [box(pos=(x[0],x[1], 0), size=(1, 1, 1), **bloco) for x in list 1] 
+                                                                            ^
+SyntaxError: invalid syntax
+'''},
