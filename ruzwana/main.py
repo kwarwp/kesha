f694d518d67d0fc4c0f7fd00a6887b3ac0262038
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 17:17:21 -0300

    Created ruzwana/main.py at Timestamp: 2018-09-24 20:17:21

---
+++ ruzwana/main.py
@@ -0,0 +1,24 @@
+# kesha.ruzwana.main.py
+# kesha.libby.main.py
+from browser import doc, html
+from _spy.vpython.main import *
+doc['pydiv'].html=''
+_gs=Glow('pydiv')
+scene=canvas()
+scene.background= color.black #"rgba(0, 0, 200, 0.5)"
+scene.width = 1200
+scene.height = 800
+bloco = dict(color=color.blue)
+bloco1 = dict(color=color.white)
+
+
+cubos1 = [box(pos=(-0.2, 1.2, x * (1.2) - 0), size=(1, 1, 1), **bloco) for x in range(4)] 
+
+
+
+
+
+
+
+
+
