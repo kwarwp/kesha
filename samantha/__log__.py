01eeedde9d3cd4a4c65f054ba3084864023868d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-23 23:49:58 -0300

    Saved samantha/__log__.py at Timestamp: 2018-09-24 02:49:58

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -816,3 +816,19 @@ NameError: name 'bloco' is not defined
 'error': '''
 Exception: <TypeError: Cannot read property 'canvas_selected' of undefined>
 '''},
+{'date': 'Sun Sep 23 2018 23:50:00.895 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 18
+    doc['py3d'].html = ''
+KeyError: py3d
+'''},
