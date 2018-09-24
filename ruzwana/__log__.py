62566123cdcedf8b9e224a13b65dff2f0cf29a27
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-24 17:49:56 -0300

    Saved ruzwana/__log__.py at Timestamp: 2018-09-24 20:49:55

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -48,3 +48,10 @@ SyntaxError: invalid syntax
                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 24 2018 17:49:54.218 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  ball = sphere[(pos=x[0], x[1], 0), radius=0.3, **bloco1) for x in list2]
+                                    ^
+SyntaxError: invalid syntax
+'''},
