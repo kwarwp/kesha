50971a2312e0829e7aadeb40f0b7fec8287f3da2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-28 20:41:54 -0300

    Saved kellee/__log__.py at Timestamp: 2018-09-28 23:41:54

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -846,3 +846,21 @@ TypeError: __init__() got an unexpected keyword argument '_3da'
     proximafase3 = Tabuleiro(**FASE3)
 TypeError: __init__() got an unexpected keyword argument '_3da'
 '''},
+{'date': 'Fri Sep 28 2018 20:41:50.136 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 29
+    class Tabuleiro:
+  module <module> line 65
+    def __init__(self, nome="esquerda", numcartas=12, lado="e", linha=4, _3d=true):
+NameError: name 'true' is not defined
+'''},
