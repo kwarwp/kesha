c1bed3ee5708c334a38e421e300896a55260580c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-28 20:15:13 -0300

    Saved kellee/__log__.py at Timestamp: 2018-09-28 23:15:12

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -806,3 +806,23 @@ SyntaxError: invalid syntax
     self.tabuleiro.empilha(self)
 AttributeError: 'Tabuleiro' object has no attribute 'empilha'
 '''},
+{'date': 'Fri Sep 28 2018 20:15:08.844 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 213
+    Jogo()
+  module <module> line 200
+    self.reset()
+  module <module> line 210
+    proximafase4 = Tabuleiro(**FASE4)
+TypeError: __init__() got an unexpected keyword argument '_3da'
+'''},
