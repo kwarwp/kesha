6043ab416139611131d07f275c6f38fced488153
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 20:43:07 -0300

    Created alexa/__log__.py at Timestamp: 2018-10-17 23:43:07

---
+++ alexa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Oct 17 2018 20:43:02.955 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  class PreviaDoMomento:
+  ^
+IndentationError: expected an indented block
+'''},
