f346eb104a5c0859c7de0af23c2f6e4f762f120d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 18:42:42 -0300

    Saved amanda/main.py at Timestamp: 2018-10-17 21:42:42

--- amanda/main.py
+++ amanda/main.py
@@ -23,9 +23,7 @@
 Gerador de labirintos e jogos tipo 'novel
 https://github.com/carlotolla/vitollino
 """
-from _spy.yoda.main import STYLE, INVENTARIO, Cena, Elemento
-from _spy.yoda.main import Texto
-from _spy.yoda.main import JOGO
+from _spy.yoda.main import STYLE, INVENTARIO, Cena, Elemento, Texto, JOGO
 from browser import window, html, document
 document["pydiv"].style.width="800px"
 document["pydiv"].style.minHeight="600px"
