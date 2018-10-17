ad121503b35ee3f633bcbb0e1ee784a52d01b6c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-10-17 20:45:51 -0300

    Saved alexa/__log__.py at Timestamp: 2018-10-17 23:45:51

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Oct 17 2018 20:45:49.56 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  debug = False
+  ^
+IndentationError: unexpected indent
+'''},
