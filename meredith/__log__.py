674c9e98482b8aa8fe07dc0a6125164c3a8ab7d0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-05 14:46:23 -0200

    Created meredith/__log__.py at Timestamp: 2019-02-05 16:46:21

---
+++ meredith/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Feb 04 2019 14:46:15.210 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from meredith import 
+                       ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
