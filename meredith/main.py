2567743aef752e4fd574b367c8422165d19a383f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-05 14:46:07 -0200

    Created meredith/main.py at Timestamp: 2019-02-05 16:46:07

---
+++ meredith/main.py
@@ -0,0 +1,11 @@
+# kesha.meredith.main.py
+from meredith import 
+
+
+class Componente():
+    def __init__(self, nome):
+        self.nome = nome
+    
+if __name__ == "__main__":
+    c = Componente("qq")
+    print(c)
