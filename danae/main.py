7643aea3d153cdc16009ef3df0625342aa8a3ee4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-02-05 14:49:18 -0200

    Saved danae/main.py at Timestamp: 2019-02-05 16:49:15

--- danae/main.py
+++ danae/main.py
@@ -10,7 +10,7 @@ class noh(Componente):
     def __init__(self, nome):
         super().__init__(nome)
         self.nome = nome
-   
+
 if __name__ == "__main__":
     n = noh("nn")
     print(n.nome)
