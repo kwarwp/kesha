146b1276e7721ce56cd567d3b54159adbf421140
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-24 19:06:41 -0300

    Saved rachel/__log__.py at Timestamp: 2019-04-24 22:06:41

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -17,3 +17,21 @@
     cenaAgua = Cena(img = "https://i.imgur.com/7xC9bET.jpg")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Wed Apr 24 2019 19:06:32.18 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    cicloagua()
+  module <module> line 12
+    cenaAgua = Cena(img = "https://i.imgur.com/7xC9bET.jpg")
+TypeError: 'module' object is not callable
+'''},
