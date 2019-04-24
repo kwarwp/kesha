7197d96894dd8b7ce14dce79ebf0f9ed16701dd7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-24 19:05:16 -0300

    Saved rachel/main.py at Timestamp: 2019-04-24 22:05:15

--- rachel/main.py
+++ rachel/main.py
@@ -6,3 +6,9 @@ cervo = "https://i.imgur.com/1ymK1ct.jpg"
 india ="https://i.imgur.com/B6MiL1Y.jpg"
 indio ="https://i.imgur.com/acNsNoO.png"
 indios ="https://i.imgur.com/RMc3y2v.png"
+
+
+def cicloagua():
+    cenaAgua = Cena(img = "https://i.imgur.com/7xC9bET.jpg")
+    cenaAgua.vai
+cicloagua()
