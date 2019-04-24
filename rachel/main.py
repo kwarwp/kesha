ffd80882d83d723db059a31ed7a3f962a2e4d04a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-24 18:44:16 -0300

    Created rachel/main.py at Timestamp: 2019-04-24 21:44:16

---
+++ rachel/main.py
@@ -0,0 +1,8 @@
+# kesha.rachel.main.py
+from _spy.vittolino.main import Cena, Elemento, Texto
+cicloagua = "https://i.imgur.com/7xC9bET.jpg"
+urso = "https://i.imgur.com/UvNnOfq.png"
+cervo = "https://i.imgur.com/1ymK1ct.jpg"
+india ="https://i.imgur.com/B6MiL1Y.jpg"
+indio ="https://i.imgur.com/acNsNoO.png"
+indios ="https://i.imgur.com/RMc3y2v.png"
