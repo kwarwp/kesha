76ebd11bbf549aa53ce53bac6c647a940f729898
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-24 20:21:38 -0300

    Saved rachel/__log__.py at Timestamp: 2019-04-24 23:21:38

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -160,3 +160,21 @@ SyntaxError: invalid syntax
                                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Apr 24 2019 20:21:28.582 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 44
+    cicloagua()
+  module <module> line 26
+    elementoindio.entra(cenaAgua)
+UnboundLocalError: local variable 'elementoindio' referenced before assignment
+'''},
