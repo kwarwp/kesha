aa9b4c4290829f414c067e2ffd16edd57e7d78be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-24 19:09:21 -0300

    Saved rachel/main.py at Timestamp: 2019-04-24 22:09:20

--- rachel/main.py
+++ rachel/main.py
@@ -1,6 +1,6 @@
 # kesha.rachel.main.py
-from _spy.vittolino.main import Cena, Elemento, Texto
-cicloagua = "https://i.imgur.com/7xC9bET.jpg"
+from _spy.vitolino.main import Cena, Elemento, Texto
+#cicloagua = "https://i.imgur.com/7xC9bET.jpg"
 urso = "https://i.imgur.com/UvNnOfq.png"
 cervo = "https://i.imgur.com/1ymK1ct.jpg"
 india ="https://i.imgur.com/B6MiL1Y.jpg"
@@ -10,5 +10,5 @@ indios ="https://i.imgur.com/RMc3y2v.png"
 
 def cicloagua():
     cenaAgua = Cena(img = "https://i.imgur.com/7xC9bET.jpg")
-    cenaAgua.vai
+    cenaAgua.vai()
 cicloagua()
