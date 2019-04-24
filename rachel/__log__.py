f24c094847f2827e1c6e96932d4f6aea8ca197f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-24 19:28:08 -0300

    Saved rachel/__log__.py at Timestamp: 2019-04-24 22:28:08

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -53,3 +53,21 @@ TypeError: 'module' object is not callable
     cenaAgua = Cena(img = "https://i.imgur.com/7xC9bET.jpg")
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Wed Apr 24 2019 19:27:59.68 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    cicloagua()
+  module <module> line 17
+    elementoindia.entrar(cenaAgua)
+AttributeError: 'Elemento' object has no attribute 'entrar'
+'''},
