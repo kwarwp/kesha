2c8f2bfab7eb03a74f46137cd5c7c54b2a4b656b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-24 20:50:40 -0300

    Saved rachel/main.py at Timestamp: 2019-04-24 23:50:39

--- rachel/main.py
+++ rachel/main.py
@@ -1,12 +1,12 @@
 # kesha.rachel.main.py
-from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 ciencia = "https://i.imgur.com/7xC9bET.jpg"
 urso = "https://i.imgur.com/UvNnOfq.png"
-cervo = "https://i.imgur.com/1ymK1ct.jpg"
-india ="https://i.imgur.com/B6MiL1Y.jpg"
 indio ="https://i.imgur.com/acNsNoO.png"
 indios ="https://i.imgur.com/RMc3y2v.png"
 
+STYLE ["width"] = 1150
+STYLE ["height"] = "600px"
 
 def cicloagua():
     cenaAgua = Cena(img = ciencia)
