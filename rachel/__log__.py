5244e55a1d4c5b543c59d34739aae0f5a1d7ac7c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-04-24 19:40:05 -0300

    Saved rachel/__log__.py at Timestamp: 2019-04-24 22:40:04

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -107,3 +107,21 @@ AttributeError: 'str' object has no attribute 'entrar'
     elementoindia.entrar(cenaAgua)
 AttributeError: 'Elemento' object has no attribute 'entrar'
 '''},
+{'date': 'Wed Apr 24 2019 19:39:54.641 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    cicloagua()
+  module <module> line 20
+    textourso.vai = urso.vai
+AttributeError: 'str' object has no attribute 'vai'
+'''},
