dff0c17334dcb2c3ae28daf73c3ba3760de605fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-03 16:24:53 -0300

    Saved soraya/main.py at Timestamp: 2019-06-03 19:24:52

--- soraya/main.py
+++ soraya/main.py
@@ -8,7 +8,7 @@ losango3d = "https://i.imgur.com/WTNCinT.png"
 estrelabugadona = "https://i.imgur.com/Yq2K158.png"
 circunferecia = "https://i.imgur.com/fI39ptw.png"
 torre = "https://i.imgur.com/a7GrJH8.png"
-STYLE ["width"] = 1000
+STYLE ["width"] = 1150
 STYLE ["height"] = "600px"
 
 def geometria():
