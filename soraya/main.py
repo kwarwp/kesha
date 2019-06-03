b35ca5855f294972f36518e217ef6b80047db189
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-06-03 16:23:48 -0300

    Created soraya/main.py at Timestamp: 2019-06-03 19:23:48

---
+++ soraya/main.py
@@ -0,0 +1,38 @@
+# kesha.soraya.main.py
+from _spy.vitollino.main import Cena, Elemento, STYLE, Texto
+fundo = "https://i.imgur.com/F3E6Lv4.jpg"
+quadrado = "https://i.imgur.com/C8Wwcse.png"
+piramide = "https://i.imgur.com/Bvjihxr.gif"
+prisma = "https://i.imgur.com/EYe69dg.png"
+losango3d = "https://i.imgur.com/WTNCinT.png"
+estrelabugadona = "https://i.imgur.com/Yq2K158.png"
+circunferecia = "https://i.imgur.com/fI39ptw.png"
+torre = "https://i.imgur.com/a7GrJH8.png"
+STYLE ["width"] = 1000
+STYLE ["height"] = "600px"
+
+def geometria():
+    fundogeo = Cena(img = fundo)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    fundogeo.vai()   
+geometria()
