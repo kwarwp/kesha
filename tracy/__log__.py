9b0675c00601d00af8540e9345578f788a4667e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-07 14:15:12 -0300

    Created tracy/__log__.py @2019-10-07 17:15

---
+++ tracy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 07 2019 14:14:01.810 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 77
+  class Perigo:
+  ^
+IndentationError: expected an indented block
+'''},
