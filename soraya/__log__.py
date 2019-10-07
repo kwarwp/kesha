81a0fa4a95deef6a0fa52cc56b86d3cfc9d1ee9e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-07 14:08:27 -0300

    Saved soraya/__log__.py @2019-10-07 17:08

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Oct 07 2019 14:07:18.943 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 39
+  fundogeo.vai()   
+  ^
+IndentationError: unexpected indent
+'''},
