1e5f662637766bdf6948491f38e0c2f65e3d347c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-07 14:08:03 -0300

    Created soraya/__log__.py @2019-10-07 17:08

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Oct 07 2019 14:06:52.515 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 39
+  fundogeo.vai()   
+  ^
+IndentationError: unexpected indent
+'''},
