8166b6b590224b4e324f9048e95e8ae65530b8a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-07 14:04:26 -0300

    Saved callie/__score__.py @2019-10-07 17:04

--- callie/__score__.py
+++ callie/__score__.py
@@ -1,1 +1,2 @@
   {'doc_id': '507c01c1', 'carta': 'carta_99', 'casa': 'centro_9_9', 'move': 'INICIA', 'ponto': 0, 'valor': 'N', '_level': 1, 'tempo': 1536107928227},
+  {'doc_id': 'b42003d3', 'carta': 'carta_99', 'casa': 'centro_9_9', 'move': 'INICIA', 'ponto': 0, 'valor': 'N', '_level': 1, 'tempo': 1570467797543},
