30e83faa25c00e1565c98049499925db6709fa48
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-10 18:11:57 -0300

    Created grace/__log__.py @2019-10-10 21:11

---
+++ grace/__log__.py
@@ -0,0 +1,21 @@
+
+{'date': 'Thu Oct 10 2019 18:11:51.883 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 135
+    jogo = JogoTesouroInca()
+  module <module> line 122
+    self.acampamento = Acampamento(ACAMPAMENTO)
+  module <module> line 108
+    self.pedras = [Elemento(
+TypeError: 'module' object is not callable
+'''},
