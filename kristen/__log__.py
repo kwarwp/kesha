aff3c0a35499b2cbe8ecac431ae6675311a6e512
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-12 16:15:57 -0300

    Created kristen/__log__.py @2019-10-12 19:15

---
+++ kristen/__log__.py
@@ -0,0 +1,5 @@
+
+{'date': 'Sat Oct 12 2019 16:15:55.896 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <TypeError: Cannot read property '$methods' of undefined>
+'''},
