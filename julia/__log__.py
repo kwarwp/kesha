b00cddc9cc6e467457fcf6a2c6d74da38821da94
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-12 16:35:49 -0300

    Saved julia/__log__.py @2019-10-12 19:35

--- julia/__log__.py
+++ julia/__log__.py
@@ -341,3 +341,19 @@ NameError: name 'entra' is not defined
     self.pedras = [Elemento(
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Sat Oct 12 2019 16:35:48.722 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 1
+    mary_shaw.roxanne.main.py
+NameError: name 'mary_shaw' is not defined
+'''},
