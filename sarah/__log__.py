d5451fe302718e0674a5f50aac3671dc32c060c6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 09:53:25 -0300

    Saved sarah/__log__.py @2019-11-06 12:53

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -100,3 +100,24 @@ Traceback (most recent call last):
     self.sai()
 AttributeError: 'Camara' object has no attribute 'sai'
 '''},
+{'date': 'Wed Nov 06 2019 10:53:31.28 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Uma expedição para coletar os tesouros do Templo Inca
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 71
+    TemploInca().inicia()
+  module <module> line 68
+    self.camara.entra(self.explorador)
+  module <module> line 53
+    self.sai()
+AttributeError: 'Camara' object has no attribute 'sai'
+'''},
