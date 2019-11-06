e1520506a0925492d4f50d5f3fc9e3c4a2223ba6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 10:54:38 -0300

    Saved sarah/__log__.py @2019-11-06 13:54

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -149,3 +149,23 @@ IndentationError: unexpected indent
                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 06 2019 11:54:43.899 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 113
+    TemploInca().inicia()
+  module <module> line 81
+    self.camara = Camara()
+  module <module> line 46
+    self.decide_entrar["s"] = self.entra #qualquer outra desição ele sai
+AttributeError: 'Camara' object has no attribute 'decide_entrar'
+'''},
