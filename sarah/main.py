1f83c287fdf1033241e6770db431d55207478ef0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-06 09:33:46 -0300

    Created sarah/main.py @2019-11-06 12:33

---
+++ sarah/main.py
@@ -0,0 +1,70 @@
+# kesha.sarah.main.py
+# mary_shaw.samantha.main.py
+from random import randint
+"""
+Uma expedição para coletar os tesouros do Templo Inca
+ --Relato:
+ fui e voltei rico
+"""
+
+__author__ = "Carlo E T Oliveira <carlo at nce ufrj br>"
+
+
+class Explorador:
+    """ explora o templo inca"""
+    def __init__(self):  # (self, camara)
+        self.mochila = 0
+        self.cabana = 0
+        # self.camara = camara?
+            
+    def pega(self, quantidade, camara):
+        """ coloca um tesouro na mochila """
+        print(f"Você coloca {quantidade} tesouro na mochila ")
+        self.mochila += quantidade
+        print(f"Você fica com {self.mochila} tesouros na mochila ")
+        camara.entra(self)
+                    
+    def sai(self):
+        """ sai do templo """
+        print("Você sai do templo e guarda os tesouros!")
+        self.cabana, self.mochila = self.mochila, 0
+        print(f"Você ficou com {self.cabana} tesouros na cabana!")
+
+
+class Camara:
+    def __init__(self):
+        self.quantidade = 3
+        #self.explorador = explorador
+        
+    def entra(self, explorador):
+        """ entra em uma câmara"""
+        print("Você entra em uma câmara com tesouros!")
+        if input("Continua?").lower() == "s":
+            if self.quantidade:
+                self.quantidade -= 1        
+                explorador.pega(randint(1, 4), self)
+            else:
+                print("Não havia mais tesouros!")
+                explorador.sai()
+        else:
+            self.sai()
+        
+
+
+class TemploInca:
+    def __init__(self):
+        self.explorador = Explorador()
+        self.camara = Camara()
+        
+    def inicia(self):
+        """ inicia a exploração """
+        print("Uma expedição para coletar os tesouros do Templo Inca")
+        self.camara.entra(self.explorador)
+        
+        
+    
+
+
+if __name__ == "__main__":
+    TemploInca().inicia()
+
