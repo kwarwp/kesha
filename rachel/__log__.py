b56569da2bd0de8be470ef665b27d402b34d16f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 15:16:53 -0300

    Saved rachel/__log__.py @2019-11-14 18:16

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -280,3 +280,10 @@ AttributeError: 'str' object has no attribute 'vai'
     terraMorta.entra()
 AttributeError: 'str' object has no attribute 'entra'
 '''},
+{'date': 'Thu Nov 14 2019 16:16:53.212 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  urso = https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjxhvPIp-rlAhU7D7kGHZbaCL0QjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26rct%3Dj%26q%3D%26esrc%3Ds%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fimagensemoldes.com.br%252Fimagens-de-personagens-masha-e-o-urso-png-8%252F%26psig%3DAOvVaw1S8_rXIbSwfxwncCCF9u9j%26ust%3D1573841746132699&psig=AOvVaw1S8_rXIbSwfxwncCCF9u9j&ust=1573841746132699
+               ^
+SyntaxError: invalid syntax
+'''},
