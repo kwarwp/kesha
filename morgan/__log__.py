8e317e656a82d75b0d6d33e37dde9b6455bac9cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 14:54:58 -0300

    Saved morgan/__log__.py @2019-12-05 17:54

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -472,3 +472,23 @@ IndentationError: unexpected indent
     self.vai = self.move
 AttributeError: 'Personagem3' object has no attribute 'move'
 '''},
+{'date': 'Thu Dec 05 2019 13:54:49.912 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 94
+    Basico()
+  module <module> line 89
+    self.menino = Personagem3(BOY, destino=self.cesta, cena=cena)
+  module <module> line 53
+    self.vai = self.move
+AttributeError: 'Personagem3' object has no attribute 'move'
+'''},
