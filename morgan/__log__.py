bc01579dda68063322feb12189039d122ca88e35
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 11:00:52 -0300

    Saved morgan/__log__.py @2019-12-05 14:00

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -103,3 +103,21 @@ IndentationError: unexpected indent
     self.play = Elemento(PLAY, x=570, y=470,w=180,h=120, cena=gameInicio, vai = self.mostradia)
 NameError: name 'PLAY' is not defined
 '''},
+{'date': 'Thu Dec 05 2019 13:00:49.699 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    Basico()
+  module <module> line 63
+    self.doggie = Personagem(DOG, destino=self.cart, cena=cena)
+AttributeError: 'Basico' object has no attribute 'cart'
+'''},
