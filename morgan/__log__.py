214bdff547873cb93ff8c99630821aab6ac4012c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 08:41:07 -0300

    Created morgan/__log__.py @2019-12-05 11:41

---
+++ morgan/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Dec 05 2019 10:41:03.885 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  gameInicio():
+               ^
+SyntaxError: invalid syntax
+'''},
