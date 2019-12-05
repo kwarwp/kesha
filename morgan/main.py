71eb715198ce37299f1058d26731f906dfc2af22
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 09:21:42 -0300

    Saved morgan/main.py @2019-12-05 12:21

--- morgan/main.py
+++ morgan/main.py
@@ -9,9 +9,9 @@ STYLE ["height"] = "600px"
 IGR = "https://i.imgur.com/"
 CEST, DOG, BASE, CENA, CENAINICIO  = f"{IGR}qtw6IoO.png", f"{IGR}ZQ9SSMz.png", f"", f"{IGR}zRGdYRp.gif", f"{IGR}3qdowNm.jpg"
 FUNDODIA, BILHETE, BOTAO, LOGO, PLAY = f"{IGR}zRGdYRp.gif", f"{IGR}p9SteRs.png", f"{IGR}kTocYiF.png", f"{IGR}JflnamW.png",f"{IGR}Jcnz4vj.png"
+
 class gameInicio:
     def __init__(self):
-        
         gameInicio = Cena(CENAINICIO)
         gameInicio.vai()
         dark= Elemento("",style=dict(width="1345px",height="600px",backgroundColor="black",opacity=0.7),cena=gameInicio)
@@ -37,6 +37,24 @@ class gameInicio:
         self.musB.x= -1200
     
     def elevador(self):
-        pass
+        todos = Cena(FUNDODIA)
+        todos.vai()
+        self.musica = Musica(TRACK)
+        self.musica.sound.pause()
+        self.musA = Elemento(SOMA, x=1200, y=500,w=80,h=80, cena=todos, vai=self.toca)
+        self.musB = Elemento(SOMB, x=-1200, y=500,w=80,h=80, cena=todos, vai=self.pause)
+"""
+class Basico:
+    def __init__(self):
+        self.cena = cena = Cena(CENA)
+        self.base0 = Plataforma("", x=500, y= 100, cena=cena)
+        self.base1 = Plataforma("", x=500, y= 500, cena=cena)
+        self.base0.destino, self.base1.destino = self.base1, self.base0 
+        self.cesta = Veiculo(CEST, destino=self.base1, cena=cena)
+        self.cesta.entra(self.base0)
+        self.doggie = Personagem(DOG, destino=self.cart, cena=cena)
+        cena.vai()
+"""
+
 
 gameInicio()
