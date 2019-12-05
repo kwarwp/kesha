f9a72839f9500de3dfd69eeec9c8820bb5a7adc8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 08:42:19 -0300

    Saved morgan/__log__.py @2019-12-05 11:42

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -60,3 +60,21 @@ NameError: name 'CENAINICIO' is not defined
     gameInicio = Cena(CENAINICIO)
 NameError: name 'CENAINICIO' is not defined
 '''},
+{'date': 'Thu Dec 05 2019 10:42:15.715 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 21
+    gameInicio()
+  module <module> line 18
+    self.logotipo = Elemento(LOGO, x=370, y=30,w=650,h=400, cena=gameInicio)
+NameError: name 'LOGO' is not defined
+'''},
