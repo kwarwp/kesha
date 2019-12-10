cc994183116446e211934bf677161f78e0c8200f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-10 12:33:52 -0300

    Saved morgan/main.py @2019-12-10 15:33

--- morgan/main.py
+++ morgan/main.py
@@ -17,8 +17,8 @@ class Predio(Elemento): #predio que  inicia bom e no fim fica queimado
         super().__init__(imagem, x= 350, y=180, w=650, h=350)
         self.nome = "casa"
 
-
-class Plataforma(Elemento): #retangulo azul
+#Lado esquerdo
+class Plataforma(Elemento): #retangulo tranparente
     def __init__(self, imagem, cena, x=400, y=0):
         super().__init__(imagem, cena=cena, w=200, x=x, y=y)
         self.destino = self
@@ -35,7 +35,11 @@ class Personagem(Elemento): #dog
         self.vai = self.move
         
     def move(self, evento=None):
+        #input(isinstance(self.destino,Veiculo))
         self.entra(self.destino)
+        self.x=0
+        self.y=0
+
 
 class Personagem2(Elemento): #Irma no predio
     def __init__(self, imagem, destino, cena, x=580, y=70):
@@ -45,6 +49,8 @@ class Personagem2(Elemento): #Irma no predio
 
     def move(self, evento=None):
         self.entra(self.destino)
+        self.x=0
+        self.y=0
 
 class Personagem3(Elemento): #garoto no predio
     def __init__(self, imagem, destino, cena, x=650, y=50):
@@ -54,6 +60,8 @@ class Personagem3(Elemento): #garoto no predio
 
     def move(self, evento=None):
         self.entra(self.destino)
+        self.x=0
+        self.y=0
 
 
 class Veiculo(Elemento): #cesta da esquerda
@@ -73,8 +81,9 @@ class Veiculo(Elemento): #cesta da esquerda
     def movimenta(self, destino):
         destino.move(self)
 
+#Lado direito
 class Veiculo2(Elemento): #cesta da direita
-    def __init__(self, imagem, destino, cena, x=500, y=0):
+    def __init__(self, imagem, destino, cena, x=300, y=0):
         super().__init__(imagem, cena=cena, x=x, y=y)
         self.nome = "veiculo2"
         self.destino = destino
@@ -101,15 +110,21 @@ class Basico:
         self.base1 = Plataforma(BASE, y=440, cena=cena)
         self.base0.destino, self.base1.destino = self.base1, self.base0 
         
-        self.cesta = Veiculo(CEST, destino=self.base1, cena=cena)
-        self.cesta.entra(self.base0)
+        self.cesta = Veiculo(CEST, destino=self.base1, cena=self.base0)
+        #self.cesta.entra(self.base0)
                       
         self.doggie = Personagem(DOG, destino=self.cesta, cena=cena)
         self.menina = Personagem2(GIRL, destino=self.cesta, cena=cena)
         self.menino = Personagem3(BOY, destino=self.cesta, cena=cena)
         
-        self.cesta2 = Veiculo2(CEST, destino=self.base1, cena=cena)
-        self.cesta2.entra(self.base0)
+        #Lado direito
+                
+        self.base0a = Plataforma(BASE, y=200, cena=cena)
+        self.base1a = Plataforma(BASE, y=440, cena=cena)
+        self.base0a.destino, self.base1a.destino = self.base1a, self.base0a 
+                   
+        self.cesta2 = Veiculo2(CEST, destino=self.base1a, cena=cena)
+        self.cesta2.entra(self.base0a)
         cena.vai()
         
         
